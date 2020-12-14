9cd8dbfa330206bc6daffc78331c7768506aedc2
PatrikLeg <PatrikLeggeri@web.de>
2020-12-14 15:14:39 +0100

    UserInfo

--- src/main/java/basic/KaffeeAutomat.java
+++ src/main/java/basic/KaffeeAutomat.java
@@ -3,22 +3,20 @@ package basic;
 public class KaffeeAutomat {
     public static void main(String[] arg) {
 
+        // preis fuer den Kaffee 30cent
         int preis = 30;
 
+        // man hat 2Euro gegeben
         int gegeben = 200;
 
-        int rest;
+        // rechnung fuer das Rueckgeld
+        int rest = gegeben - preis;
 
-        rest = gegeben - preis;
-
+        // rechnung fuer Cent
         rest = rest % 100;
 
+        // ausgabe Euro und Cent
         System.out.println("Du bekommst 1Euro und " + rest + "cent wieder.");
-
-
-
-
-
 
     }
 }
