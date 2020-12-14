fa723455d4b1cee6341bb63e2dc8fd4e61286b74
Vladimir09-pZ <reh.vlad@hotmail.com>
2020-12-14 13:21:55 +0100

    userinfo

---
+++ src/main/java/basic/aufgabe1/Userinfo.java
@@ -0,0 +1,8 @@
+package basic.aufgabe1;
+
+public class Userinfo {
+    public static void main(String[] args) {
+        System.out.println("Vladimir Reh");
+        System.out.println("Consorsbank");
+    }
+}
