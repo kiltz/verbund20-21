433210c1c895f0380e5ba5b5710886c29bbec789
AlexBleicher <alexander.bleicher01@gmail.com>
2020-12-14 15:35:38 +0100

    Aufgabe 2 aber richtig

--- src/main/java/basic/aufgabe2/KaffeeAutomat.java
+++ src/main/java/basic/aufgabe2/KaffeeAutomat.java
@@ -4,11 +4,7 @@ public class KaffeeAutomat {
     public static void main(String[] args) {
         int preis = 30;
 
-<<<<<<< HEAD
         int gegeben=500;
-=======
-        int gegeben=200;
->>>>>>> origin/AlexanderBleicher
 
         int rueck=gegeben-preis;
 
@@ -21,7 +17,7 @@ public class KaffeeAutomat {
         int rest2ctMuenzen=0;
         int rest1ctMuenzen=0;
         while(rueck>0) {
-<<<<<<< HEAD
+
             System.out.println(rueck);
             if (rueck >=200) {
                 rest2EuroMuenzen=rueck/200;
@@ -52,36 +48,6 @@ public class KaffeeAutomat {
                 rueck--;
             }
 
-=======
-            if (rueck >=200) {
-                rest2EuroMuenzen++;
-                rueck = rueck % 200;
-            } else if (rueck >= 100) {
-                rest1EuroMuenzen++;
-                rueck = rueck % 100;
-            } else if (rueck >= 50) {
-                rest50ctMuenzen++;
-                rueck = rueck % 50;
-            }
-            else if(rueck>=20){
-                rest20ctMuenzen++;
-                rueck=rueck%20;
-            }
-            else if (rueck >= 10) {
-                rest10ctMuenzen++;
-                rueck = rueck % 10;
-            } else if (rueck >= 5) {
-                rest5ctMuenzen++;
-                rueck = rueck % 5;
-            } else if (rueck >= 2) {
-                rest2ctMuenzen++;
-                rueck = rueck % 2;
-            }
-            else {
-                rest1ctMuenzen++;
-                rueck--;
-            }
->>>>>>> origin/AlexanderBleicher
         }
         System.out.println("2€: " + rest2EuroMuenzen + " 1€: " + rest1EuroMuenzen +" 50ct: " + rest50ctMuenzen + " 20ct: " + rest20ctMuenzen + "  10ct: " + rest10ctMuenzen + " 5ct:" + rest5ctMuenzen + " 2ct: " + rest2ctMuenzen + " 1ct: " + rest1ctMuenzen);
     }
