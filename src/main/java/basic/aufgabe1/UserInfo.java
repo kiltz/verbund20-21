aa8e07ccff5d2a2a365c09f200c680253d46837b
pauleman1 <paul.hofmann@ingentis.com>
2020-12-14 13:18:28 +0100

    Userinfo Paul

---
+++ src/main/java/basic/aufgabe1/UserInfo.java
@@ -0,0 +1,8 @@
+package basic.aufgabe1;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        System.out.println("Paul Hofmann");
+        System.out.println("Ingentis Softwareentwicklung");
+    }
+}
