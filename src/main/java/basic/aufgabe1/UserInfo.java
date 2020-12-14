b04b317bf1c2a16ade7baab988bbba08e1e7c37a
Michael Barbat <mikeybarbat@gmail.com>
2020-12-14 13:50:36 +0100

    Letsssssssssss go

--- src/main/java/basic/aufgabe1/UserInfo.java
+++ src/main/java/basic/aufgabe1/UserInfo.java
@@ -2,6 +2,7 @@ package basic.aufgabe1;
 
 public class UserInfo {
     public static void main(String[] args) {
+        System.out.println("Michael Barbat");
         System.out.println("Consorsbank beste Bank");
 
     }
