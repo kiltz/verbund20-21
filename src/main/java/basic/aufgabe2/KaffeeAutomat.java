34bd873136b3948345193ad35ccf59e8a141b0fe
pauleman1 <paul.hofmann@ingentis.com>
2020-12-14 15:12:55 +0100

    KaffeAutomat Paul

--- src/main/java/basic/aufgabe2/KaffeeAutomat.java
+++ src/main/java/basic/aufgabe2/KaffeeAutomat.java
@@ -3,18 +3,33 @@ package basic.aufgabe2;
 public class KaffeeAutomat {
 
 	public static void main(String[] args) {
-		// Angenommen: Ein Kaffee kostet 30 Cent,
-		// wir werfen eine 2-Euro Münze in den Automat
-		// Welche Münzen bekommen wir als Rückgeld?
 
-		// 1. definiere den Preis und das gezahlte Geld am Anfang der Methode
-
-		// 2. Ermittle mit Hilfe von Modulo die Anzahl der einzelnen
-		// 		Münzen und arbeite mit dem Rest weiter.
-
-		// 3. Vereinfache die Lösung mit Schleifen
+		int preis = 30;
+		int gegeben = 200;
 
 
+
+		int rueckgeld = gegeben - preis;
+
+		System.out.println(rueckgeld);
+		//ruckgeld ausgabe
+
+
+		int euroMuenze = rueckgeld % 100; //70
+		System.out.println(euroMuenze);
+		rueckgeld = rueckgeld - euroMuenze;
+		System.out.println(rueckgeld);
+		int fuffimuenze = rueckgeld % 50;
+		rueckgeld = rueckgeld - fuffimuenze;
+		int zwannimuenze = rueckgeld % 20;
+		rueckgeld = rueckgeld -  zwannimuenze;
+		int zehnermuenze = rueckgeld % 10;
+		rueckgeld = rueckgeld -  zehnermuenze;
+		int fuenfermuenze = rueckgeld % 5;
+		rueckgeld = rueckgeld - fuenfermuenze;
+
+
+		System.out.println(rueckgeld + euroMuenze + "1 Euro" + fuffimuenze + "50-Cent )" + zwannimuenze +" 20-Cent " + zehnermuenze +"10-Cent " );
 	}
 
 }
