3d7976aae99ea8893a42604b32b39cb846ff5493
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-14 13:25:15 +0100

    Push - Class UserInfo update

--- src/main/java/basic/aufgabe1/UserInfo.java
+++ src/main/java/basic/aufgabe1/UserInfo.java
@@ -3,7 +3,7 @@ package basic.aufgabe1;
 public class UserInfo {
     public static void main(String[] args) {
         String strName = "Hallo, mein Name ist Leon Wolf. ";
-        String strFirma = "Ich arbeite bei Ergo Direkt.";
+        String strFirma = "Ich arbeite für die Ergo Direkt.";
         System.out.println(strName + strFirma);
     }
 }
