7e10b5d8f784beab39c935a3f8f98a6c0811a3ac
albartes <alexander.bartes@t-online.de>
2020-12-14 13:33:47 +0100

    pushed-user info

---
+++ src/main/java/basic/aufgabe1/UserInfo.java
@@ -0,0 +1,11 @@
+package basic.aufgabe1;
+
+import java.sql.SQLOutput;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        System.out.println("Ich bin Alexander Bartes");
+        System.out.println("Ich bin 18 Jahre alt");
+        System.out.println("Ich arbeite fuer die Ergo Direkt AG");
+    }
+}
