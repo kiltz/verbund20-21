0af9ffc6957e462db085ed84f1299eefdac5127c
Fabian Kasbeitzer <fabian2312.kasbeitzer@gmail.com>
2020-12-14 13:46:58 +0100

    Aufgabe1

--- src/main/java/basic/Aufgabe1.java
+++ src/main/java/basic/Aufgabe1.java
@@ -2,6 +2,8 @@ package basic;
 
 public class Aufgabe1 {
     public static void main(String[] args) {
-        System.out.println("Aufgabe1");
+
+        System.out.println("Fabian Kasbeitzer");
+        System.out.println("Ingentis Software Entwicklung");
     }
 }
