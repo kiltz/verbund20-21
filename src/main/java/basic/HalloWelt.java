17d9677e93d58afc174c3aa41ef0e6d38d56a746
Michael Barbat <mikeybarbat@gmail.com>
2020-12-14 13:26:48 +0100

    Letsssssssssss go

--- src/main/java/basic/HalloWelt.java
+++ src/main/java/basic/HalloWelt.java
@@ -3,5 +3,6 @@ package basic;
 public class HalloWelt {
     public static void main(String[] args) {
         System.out.println("Hallo Welt!");
+
     }
 }
---
+++ src/main/java/basic/aufgabe1/UserInfo.java
@@ -0,0 +1,8 @@
+package basic.aufgabe1;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        System.out.println("Consorsbank beste Bank");
+
+    }
+}
