31fc229e7fac5551612e60719bd85e81d78a2ef3
FabianLechner <fab.lechner@gmail.com>
2020-12-14 13:18:23 +0100

    Userinfo Aufgabe

--- src/main/java/basic/aufgabe1/UserInfo.java
+++ src/main/java/basic/aufgabe1/UserInfo.java
@@ -3,6 +3,6 @@ package basic.aufgabe1;
 public class UserInfo {
     public static void main(String[] args) {
         System.out.println("Mein Name lautet Fabian Lechner");
-        System.out.println("Ich arbeite bei der GfK");
+        System.out.println("Ich arbeite bei der GfK!");
     }
 }
