72a4139a25882cc4a1001a00bf4f44f98dce8a7a
PatrikLeg <PatrikLeggeri@web.de>
2020-12-14 13:18:45 +0100

    UserInfo

---
+++ src/main/java/basic/aufgabe1/UserInfo.java
@@ -0,0 +1,9 @@
+package basic.aufgabe1;
+
+public class UserInfo {
+    public static void main(String[] arg) {
+        System.out.println("Name: Leggeri");
+        System.out.println("Vorname: Patrik");
+        System.out.print("Firma: Consorsbank");
+    }
+}
