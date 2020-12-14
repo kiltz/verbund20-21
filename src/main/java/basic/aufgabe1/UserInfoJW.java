892c614d166d4ecba120bd1e1cc4be2653e48e44
Julian <julianwonnenberg@gmail.com>
2020-12-14 13:24:06 +0100

    Informationen in Main

---
+++ src/main/java/basic/aufgabe1/UserInfoJW.java
@@ -0,0 +1,8 @@
+package basic.aufgabe1;
+
+public class UserInfoJW {
+    public static void main(String[] args) {
+        System.out.println("Name: " + " Julian Wonnenberg");
+        System.out.println("Firma: " + " GfK");
+    }
+}
