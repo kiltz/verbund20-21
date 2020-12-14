c2127abba6ccadc7b44a8e7dd6159117e9d26d72
AlexBleicher <alexander.bleicher01@gmail.com>
2020-12-14 13:16:45 +0100

    Aufgabe 1

---
+++ src/main/java/basic/aufgabe1/UserInfo.java
@@ -0,0 +1,7 @@
+package basic.aufgabe1;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        System.out.println("Mein Name ist Alexander Bleicher. Ich arbeite bei der ERGO DIREKT AG");
+    }
+}
