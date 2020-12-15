d3afa98beabc1249a01e6904f556cbce4c778ad4
FabianLechner <fab.lechner@gmail.com>
2020-12-15 11:40:00 +0100

    Userinfo Aufgabe Wuerfelstatistik

--- src/main/java/basic/aufgabe2/WuerfelStatistik.java
+++ src/main/java/basic/aufgabe2/WuerfelStatistik.java
@@ -1,27 +1,5 @@
 package basic.aufgabe2;
 
-/**
- * Aufgabe:
- *
- * Würfle 10000 mal, nutze dazu die Klasse Math, die eine Methode random()
- * anbietet die eine Zufallszahl erzeugt.
- *
- * Wandele die Zufallszahl in eine Zahl zwischen 1 und 6 um.
- *
- * Merke Dir in einem Array wie oft welche Zahl gewürfelt wurde.
- *
- * Gebe die Statistik aus.
- *
- * Zusatzaufgabe:
- *
- * Stoppe die Zeit, die für den Durchlauf gebraucht wird.
- * ( System.currentTimeMillis() )
- * Gebe das Ergebnis in ansprechender Form aus.
- *
- * Variiere die Anzahl der Durchläufe, gehe in die Masse.
- *
- */
-
 public class WuerfelStatistik {
 	public static void main(String[] args) {
 		int[] anzahl = {0,0,0,0,0,0};
