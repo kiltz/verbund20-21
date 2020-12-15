3dc435da5a6b91c51d1a9c1e9341ef08e1cd2e9e
Fabian Kasbeitzer <fabian2312.kasbeitzer@gmail.com>
2020-12-15 11:32:47 +0100

    Aufgabe1

--- src/main/java/basic/aufgabe2/Wuerfel.java
+++ src/main/java/basic/aufgabe2/Wuerfel.java
@@ -2,13 +2,14 @@ package basic.aufgabe2;
 
 public class Wuerfel {
     public static void main(String[] args) {
-        int[] liste = {};
+        int[] liste = {0, 0, 0, 0, 0, 0};
         int würfelzahl = 1001;
         for (int würfelanzahl = 1; würfelanzahl < würfelzahl; würfelanzahl++) {
             double zufall = Math.random();
             if (zufall < 0.7) {
-                int zufallint = (int) zufall;
-                liste.add(zufallint);
+                double zufallint = zufall * 10;
+                int zufallintganz = (int) zufall;
+                System.out.println(zufallintganz);
             }
             else {
                 würfelzahl += 1;
