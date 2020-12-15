e5dadbf04d105481fddc54567f568ebbf90d229c
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-15 10:03:24 +0100

    push - WuerfelStatistik

--- src/main/java/basic/aufgabe2/WuerfelStatistik.java
+++ src/main/java/basic/aufgabe2/WuerfelStatistik.java
@@ -24,10 +24,36 @@ package basic.aufgabe2;
 
 public class WuerfelStatistik {
 	public static void main(String[] args) {
+		int[] arrayOfNumbers = new int[10000];
+		int anzahl1 = 0;
+		int anzahl2 = 0;
+		int anzahl3 = 0;
+		int anzahl4 = 0;
+		int anzahl5 = 0;
+		int anzahl6 = 0;
 
-
-		double zufall = Math.random();
-
-		// hier gehts los....
+		for(int i = 0; i < 10000; i++) {
+			int zufall = (int)(Math.random()*6+1);
+			if (zufall == 1){
+				anzahl1 += 1;
+			}
+			else if (zufall == 2){
+				anzahl2 += 1;
+			}
+			else if (zufall == 3){
+				anzahl3 += 1;
+			}
+			else if (zufall == 4){
+				anzahl4 += 1;
+			}
+			else if (zufall == 5){
+				anzahl5 += 1;
+			}
+			else if (zufall == 6){
+				anzahl6 += 1;
+			}
+			arrayOfNumbers[i] = zufall;
+		}
+		System.out.println("1: " + anzahl1 + ", 2: " + anzahl2 + ", 3: " + anzahl3 + ", 4: " + anzahl4 +", 5: " + anzahl5 + ", 6: " + anzahl6);
 	}
 }
