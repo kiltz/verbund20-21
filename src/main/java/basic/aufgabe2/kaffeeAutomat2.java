ab881508d81008bea2db6a250a47bd7887f2e90f
Julian <julianwonnenberg@gmail.com>
2020-12-15 10:41:30 +0100

    Kaffee Übung 2 Vereinfachte Rechnung mit Schleife

---
+++ src/main/java/basic/aufgabe2/kaffeeAutomat2.java
@@ -0,0 +1,17 @@
+package basic.aufgabe2;
+
+public class kaffeeAutomat2 {
+    public static void main(String[] args) {
+
+        int preis = 30;
+        int gegeben = 200;
+        int rest = gegeben - preis;
+        int[] wert = {100, 50, 20, 10, 5};
+
+        for (int i = 0; i < wert.length; i++) {
+            int anzahl = rest / wert[i];
+            rest = rest % wert[i];
+            System.out.println(anzahl + rest);
+        }
+    }
+}
