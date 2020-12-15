9ba8ea4aa3582b742ada03034460fae432579262
albartes <alexander.bartes@t-online.de>
2020-12-15 14:44:08 +0100

    pushed-user info

---
+++ src/main/java/oop/basic/aufgabe/Konto.java
@@ -0,0 +1,27 @@
+package oop.basic.aufgabe;
+
+public class Konto {
+    private int Kontostand = 0;
+    private int dispo = 0;
+
+    public void setDispo(int einzahlen){
+       dispo = dispo + einzahlen;
+    }
+
+    public int getDispo(){
+        return dispo;
+    }
+    public int getKontostand(){
+        return Kontostand;
+    }
+    public int einzahlen(int einzahlung){
+        Kontostand += einzahlung
+    }
+
+    public int auszahlen(int auszahlung){
+        Kontostand -= auszahlung;
+    }
+
+
+}
+
