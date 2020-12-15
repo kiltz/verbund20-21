444e31d04945bb8c2d972cf99121cf07e2407a2f
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-15 10:15:44 +0100

    push - WuerfelStatistik

--- src/main/java/basic/aufgabe2/WuerfelStatistik.java
+++ src/main/java/basic/aufgabe2/WuerfelStatistik.java
@@ -2,63 +2,57 @@ package basic.aufgabe2;
 
 /**
  * Aufgabe:
- *
+ * <p>
  * Würfle 10000 mal, nutze dazu die Klasse Math, die eine Methode random()
  * anbietet die eine Zufallszahl erzeugt.
- *
+ * <p>
  * Wandele die Zufallszahl in eine Zahl zwischen 1 und 6 um.
- *
+ * <p>
  * Merke Dir in einem Array wie oft welche Zahl gewürfelt wurde.
- *
+ * <p>
  * Gebe die Statistik aus.
- *
+ * <p>
  * Zusatzaufgabe:
- *
+ * <p>
  * Stoppe die Zeit, die für den Durchlauf gebraucht wird.
  * ( System.currentTimeMillis() )
  * Gebe das Ergebnis in ansprechender Form aus.
- *
+ * <p>
  * Variiere die Anzahl der Durchläufe, gehe in die Masse.
- *
  */
 
 public class WuerfelStatistik {
-	public static void main(String[] args) {
-		int[] arrayOfNumbers = new int[10000];
-		int anzahl1 = 0;
-		int anzahl2 = 0;
-		int anzahl3 = 0;
-		int anzahl4 = 0;
-		int anzahl5 = 0;
-		int anzahl6 = 0;
+    public static void main(String[] args) {
+        int[] arrayOfNumbers = new int[10000];
+        int anzahl1 = 0;
+        int anzahl2 = 0;
+        int anzahl3 = 0;
+        int anzahl4 = 0;
+        int anzahl5 = 0;
+        int anzahl6 = 0;
 
-		for(int i = 0; i < 10000; i++) {
-			int zufall = (int)(Math.random()*6+1);
-			if (zufall == 1){
-				anzahl1 += 1;
-			}
-			else if (zufall == 2){
-				anzahl2 += 1;
-			}
-			else if (zufall == 3){
-				anzahl3 += 1;
-			}
-			else if (zufall == 4){
-				anzahl4 += 1;
-			}
-			else if (zufall == 5){
-				anzahl5 += 1;
-			}
-			else if (zufall == 6){
-				anzahl6 += 1;
-			}
-			arrayOfNumbers[i] = zufall;
-		}
-		System.out.println("1: " + anzahl1 +
-							", 2: " + anzahl2 +
-							", 3: " + anzahl3 +
-							", 4: " + anzahl4 +
-							", 5: " + anzahl5 +
-							", 6: " + anzahl6);
-	}
+        for (int i = 0; i < 10000; i++) {
+            int zufall = (int) (Math.random() * 6 + 1);
+            if (zufall == 1) {
+                anzahl1 += 1;
+            } else if (zufall == 2) {
+                anzahl2 += 1;
+            } else if (zufall == 3) {
+                anzahl3 += 1;
+            } else if (zufall == 4) {
+                anzahl4 += 1;
+            } else if (zufall == 5) {
+                anzahl5 += 1;
+            } else if (zufall == 6) {
+                anzahl6 += 1;
+            }
+            arrayOfNumbers[i] = zufall;
+        }
+        System.out.println("1: " + anzahl1 +
+                ", 2: " + anzahl2 +
+                ", 3: " + anzahl3 +
+                ", 4: " + anzahl4 +
+                ", 5: " + anzahl5 +
+                ", 6: " + anzahl6);
+    }
 }
