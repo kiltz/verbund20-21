f469bd74013f65ac338f3d7d37332caba1385485
FabianLechner <fab.lechner@gmail.com>
2020-12-15 13:50:21 +0100

    Userinfo Aufgabe Wuerfelstatistik

--- src/main/java/basic/aufgabe2/WuerfelStatistik.java
+++ src/main/java/basic/aufgabe2/WuerfelStatistik.java
@@ -7,7 +7,8 @@ public class WuerfelStatistik {
 		int min = 1;
 		int zahl = 1;
 		int range = max - min + 1;
-		int durchlaeufe = 100000;
+		int durchlaeufe = 100000000;
+
 		long zeitAnfang = System.currentTimeMillis();
 
 		for (int i = 0; i < durchlaeufe; i++) {
@@ -16,9 +17,10 @@ public class WuerfelStatistik {
 			anzahl[zufall-1] = anzahl[zufall-1]+1;
 		}
 		for (int j : anzahl) {
-			System.out.println("Die Zahl " + zahl + " wurde " + j + " gewuerfelt");
+			System.out.println("Die Zahl " + zahl + " wurde " + j + " mal gewuerfelt");
 			++zahl;
 		}
+
 		long zeitEnde = System.currentTimeMillis();
 		long zeitDif = zeitEnde - zeitAnfang;
 		System.out.println("Der Computer hat fuer die Aufgabe " + zeitDif + "ms gebraucht");
