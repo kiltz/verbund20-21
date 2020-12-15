7fe1b1ba980bacd6eefe81f9c6c2242073c89809
Vladimir09-pZ <reh.vlad@hotmail.com>
2020-12-15 11:14:42 +0100

    würfelstatistik

--- src/main/java/basic/aufgabe2/WuerfelStatistik.java
+++ src/main/java/basic/aufgabe2/WuerfelStatistik.java
@@ -23,11 +23,34 @@ package basic.aufgabe2;
  */
 
 public class WuerfelStatistik {
+
 	public static void main(String[] args) {
+		int i = 0;
+		int[] Zahlenhäufigkeit = {0, 0, 0, 0, 0, 0};
+		while (i < 10000) {
+			double zufall = Math.random();
+			i = i + 1;
+			//system.out.println(zufall);
 
+			if (zufall < 0.1666666) {
+				Zahlenhäufigkeit[0] = Zahlenhäufigkeit[0] + 1;
+			} else if (zufall < 0.3333333) {
+				Zahlenhäufigkeit[1] = Zahlenhäufigkeit[1] + 1;
+			} else if (zufall < 0.4999999) {
+				Zahlenhäufigkeit[2] = Zahlenhäufigkeit[2] + 1;
+			} else if (zufall < 0.6666666) {
+				Zahlenhäufigkeit[3] = Zahlenhäufigkeit[3] + 1;
+			} else if (zufall < 0.8333333) {
+				Zahlenhäufigkeit[4] = Zahlenhäufigkeit[4] + 1;
+			} else {
+				Zahlenhäufigkeit[5] = Zahlenhäufigkeit[5] + 1;
+			}
+		}
+		int zahl = 0;
+		for (int anzahl : Zahlenhäufigkeit) {
+			zahl = zahl + 1;
+			System.out.println(zahl + ":  " + anzahl);
+		}
 
-		double zufall = Math.random();
-
-		// hier gehts los....
 	}
 }
