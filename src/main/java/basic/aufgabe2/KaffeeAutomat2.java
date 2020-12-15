83fd3d7bb0a5d94870afca843db73b0e4104f468
FabianLechner <fab.lechner@gmail.com>
2020-12-15 09:49:41 +0100

    Userinfo Aufgabe KaffeeAutomat

--- src/main/java/basic/aufgabe2/KaffeeAutomat2.java
+++ src/main/java/basic/aufgabe2/KaffeeAutomat2.java
@@ -7,14 +7,14 @@ public class KaffeeAutomat2 {
     int preis = 30;
     int gegeben = 200;
     int rueckgeld = gegeben - preis;
-    int[] wert = new int[] {100,50, 20, 10,5};
-    int[] anzahl = new int[5];
-    String[] name = new String[] {"1-Euro", "50-Cent", "20-Cent", "10-Cent", "5-Cent"};
+    int[] wert =  {100,50, 20, 10,5};
+
+    String[] name =  {"1-Euro", "50-Cent", "20-Cent", "10-Cent", "5-Cent"};
 
     for (int i = 0; i < 5; i++) {
-        anzahl[i] = rueckgeld / wert[i];
+        int anzahl = rueckgeld / wert[i];
         rueckgeld = rueckgeld % wert[i];
-        System.out.println(" Anzahl " + name[i] +" Muenze(n): " + anzahl[i] + " Rest: " + rueckgeld );
+        System.out.println(" Anzahl " + name[i] +" Muenze(n): " + anzahl + " Rest: " + rueckgeld );
         }
     }
 }
