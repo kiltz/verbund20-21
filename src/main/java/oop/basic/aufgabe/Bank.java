2e518408cdec73f9b105780018a4b80b4e748bce
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-15 14:30:28 +0100

    push - class Konto + Bank

--- src/main/java/oop/basic/aufgabe/Bank.java
+++ src/main/java/oop/basic/aufgabe/Bank.java
@@ -7,16 +7,13 @@ public class Bank {
         if (k.getDispo() != 1500) {
             System.out.println("Fehler 1: Dispo stimmt nicht!");
         }
-
         if (k.getKontostand() != 0) {
             System.out.println("Fehler 2: Kontostand stimmt nicht!");
         }
-
         k.einzahlen(500);
         if (k.getKontostand() != 500) {
             System.out.println("Fehler 3: Kontostand stimmt nicht!");
         }
-
         k.auszahlen(200);
         if (k.getKontostand() != 300) {
             System.out.println("Fehler 4: Kontostand stimmt nicht!");
@@ -29,7 +26,5 @@ public class Bank {
         if (k.getKontostand() != -200) {
             System.out.println("Fehler 6: Kontostand stimmt nicht!");
         }
-
-
     }
 }
