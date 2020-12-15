b859dd181af3a4727baa8ad1e520567f0843ef52
PatrikLeg <PatrikLeggeri@web.de>
2020-12-15 10:32:22 +0100

    Kaffeautomat mit Schleife vereinfacht

--- src/main/java/basic/KaffeeAutomat.java
+++ src/main/java/basic/KaffeeAutomat.java
@@ -15,10 +15,10 @@ public class KaffeeAutomat {
         int werte[] = {100, 50, 20, 10, 5};
         String name[] = {"1-Euro", "50-Cent", "20-Cent", "10-Cent", "5-Cent"};
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < werte.length; i++) {
             int anzahl = rest / werte[i];
             rest = rest % werte[i];
-            System.out.println(anzahl + ": " + name[i]);
+            System.out.println("Du bekommst " + anzahl + ": " + name[i] + " Muenzen");
         }
 
 
