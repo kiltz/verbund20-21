d3399dce61a49ca9c9bd8b6cdaac6edbd1c3a380
Mikey <mikeybarbat@gmail.com>
2020-12-16 09:37:08 +0100

    Hallo

--- src/main/java/basic/HalloWelt.java
+++ src/main/java/basic/HalloWelt.java
@@ -3,6 +3,6 @@ package basic;
 public class HalloWelt {
     public static void main(String[] args) {
         System.out.println("Hallo Welt!");
-
+        System.out.println("Hallo Welt!");
     }
 }
