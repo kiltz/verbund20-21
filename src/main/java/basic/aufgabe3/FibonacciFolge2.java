cb0cdd1cbeb796e9d5b9df0e3f23b05303df6c52
PatrikLeg <PatrikLeggeri@web.de>
2020-12-16 10:07:43 +0100

    If Bedingung gestellt

--- src/main/java/basic/aufgabe3/FibonacciFolge2.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge2.java
@@ -11,9 +11,11 @@ public class FibonacciFolge2 {
 
         for(int i = 0;i < 11; i++){
             zahl3 = zahl1 + zahl2;
-            System.out.print(" " + zahl3);
-            zahl1 = zahl2;
-            zahl2 = zahl3;
+            if (zahl3 <= 100) {
+                System.out.print(" " + zahl3);
+                zahl1 = zahl2;
+                zahl2 = zahl3;
+            }
         }
     }
 }
