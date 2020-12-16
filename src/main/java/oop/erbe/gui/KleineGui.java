86650c9c3bb7261e9d49f3adee69aa017c9a66d4
Vladimir09-pZ <reh.vlad@hotmail.com>
2020-12-16 14:45:12 +0100

    1

--- src/main/java/oop/erbe/gui/KleineGui.java
+++ src/main/java/oop/erbe/gui/KleineGui.java
@@ -19,13 +19,13 @@ public class KleineGui extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         VBox box = new VBox();;
-        Label lTest = new Label("<><><><><><><><><>");
+        Label lTest = new Label("<><><><><><><><><><><><><><><><><><><><><>");
         box.getChildren().add(lTest);
         Label lTest1 = new Label("Hallo Welt") ;
         box.getChildren().add(lTest1);
         Label lTest2 = new Label("Hier bin ich, lol") ;
         box.getChildren().add(lTest2);
-        Label lTest3 = new Label("<><><><><><><><><>");
+        Label lTest3 = new Label("<><><><><><><><><><><><><><><><><><><><><>");
         box.getChildren().add(lTest3);
 
         Scene scene = new Scene(box, 400,250);
