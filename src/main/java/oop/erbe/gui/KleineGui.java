440784f83a2fd0f381e636e3c5ec15d0532b93e9
FabianKasbeitzer <fabian2312.kasbeitzer@gmail.com>
2020-12-16 14:46:20 +0100

    test

--- src/main/java/oop/erbe/gui/KleineGui.java
+++ src/main/java/oop/erbe/gui/KleineGui.java
@@ -3,9 +3,13 @@ package oop.erbe.gui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class KleineGui extends Application {
 
@@ -21,10 +25,13 @@ public class KleineGui extends Application {
         box.getChildren().add(lInfo);
         Label lStatus = new Label("Ein Status");
         box.getChildren().add(lStatus);
+        Label lInfo2 = new Label("Ein weiterer kleiner Text");
+        box.getChildren().add(lInfo2);
+        Image img = new Image("hqdefault.jpg");
+        box.getChildren().add(new ImageView(img));
 
-        Scene scene = new Scene(box, 400,250);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Mein kleines sinnloses Fensterchen");
-        primaryStage.show();
+        }
+
+
     }
-}
+
