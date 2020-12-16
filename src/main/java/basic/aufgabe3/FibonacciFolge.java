e81a8ea6d1e832ea2852d10c60bb76f369e4ca6c
julianwgit <julianwonnenberg@gmail.com>
2020-12-16 10:08:49 +0100

    Fibonacci Folge

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -3,5 +3,10 @@ package basic.aufgabe3;
 public class FibonacciFolge {
     public static void main(String[] args) {
         //Gebe die Fibonacci bis 100 aus
+        // Keine Ahnung was ich machen muss
+    }
+
+    public static int fibonacci(int n){
+        return n;
     }
 }
