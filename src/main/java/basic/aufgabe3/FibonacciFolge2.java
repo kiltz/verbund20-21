c251af44d1deb66d1fac97ab1de80c8164cf8f36
PatrikLeg <PatrikLeggeri@web.de>
2020-12-16 10:04:57 +0100

    Aufgabe erledigt

---
+++ src/main/java/basic/aufgabe3/FibonacciFolge2.java
@@ -0,0 +1,19 @@
+package basic.aufgabe3;
+
+public class FibonacciFolge2 {
+    public static void main(String[] arg) {
+
+        int zahl1 = 0;
+        int zahl2 = 1;
+        int zahl3;
+
+        System.out.print(zahl1 + " " + zahl2);
+
+        for(int i = 0;i < 11; i++){
+            zahl3 = zahl1 + zahl2;
+            System.out.print(" " + zahl3);
+            zahl1 = zahl2;
+            zahl2 = zahl3;
+        }
+    }
+}
