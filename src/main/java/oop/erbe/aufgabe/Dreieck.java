0ccd936d58adfe2462a9903298984297f9ccd12b
Vladimir09-pZ <reh.vlad@hotmail.com>
2020-12-16 13:59:42 +0100

    1

---
+++ src/main/java/oop/erbe/aufgabe/Dreieck.java
@@ -0,0 +1,9 @@
+package oop.erbe.aufgabe;
+
+public class Dreieck extends Form {
+    public int s3 = 14;
+    public Dreieck(int s1, int s2, int s3) {
+
+        super(s1,s2);
+    }
+}
---
+++ src/main/java/oop/erbe/aufgabe/Rechteck.java
@@ -0,0 +1,12 @@
+package oop.erbe.aufgabe;
+
+import oop.erbe.Auto;
+
+public class Rechteck extends Form {
+    public Rechteck() {
+        this();
+    }
+    public Rechteck(int s1, int s2) {
+        super(s1, s2)
+}
+
