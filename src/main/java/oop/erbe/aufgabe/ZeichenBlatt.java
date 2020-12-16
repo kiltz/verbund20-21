05bc81090713ec27852c82c2e003431084035ab4
Friedrich Kiltz <f@kiltz.de>
2020-12-16 13:59:47 +0100

    Vererbung und GUI

--- src/main/java/oop/erbe/aufgabe/ZeichenBlatt.java
+++ src/main/java/oop/erbe/aufgabe/ZeichenBlatt.java
@@ -1,5 +1,10 @@
 package oop.erbe.aufgabe;
 
+import oop.erbe.aufgabe.muloe.Dreieck;
+import oop.erbe.aufgabe.muloe.Form;
+import oop.erbe.aufgabe.muloe.Kreis;
+import oop.erbe.aufgabe.muloe.Rechteck;
+
 public class ZeichenBlatt {
 
     public static void main(String[] args) {
@@ -10,5 +15,9 @@ public class ZeichenBlatt {
         Form r1 = new Rechteck(10,20);
         System.out.println(r1.berechneUmfang());
 
+        Form k1 = new Kreis(14);
+        System.out.println(k1.berechneUmfang());
+
+        System.out.println(r1);
     }
 }
