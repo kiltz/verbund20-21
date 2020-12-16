dde8eabd2bb9749d6be53c2a00233acb535d24fe
Vladimir09-pZ <reh.vlad@hotmail.com>
2020-12-16 14:43:16 +0100

    1

--- src/main/java/oop/erbe/gui/KleineGui.java
+++ src/main/java/oop/erbe/gui/KleineGui.java
@@ -1,11 +1,14 @@
 package oop.erbe.gui;
 
+import com.sun.javafx.scene.control.skin.LabeledText;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javax.swing.plaf.LabelUI;
 
 public class KleineGui extends Application {
 
@@ -15,16 +18,21 @@ public class KleineGui extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        VBox box = new VBox();
-        Label test = new Label("Ein kleinerer Text");
-        Label lInfo = new Label("Ein kleiner Text");
-        box.getChildren().add(lInfo);
-        Label lStatus = new Label("Ein Status");
-        box.getChildren().add(lStatus);
+        VBox box = new VBox();;
+        Label lTest = new Label("<><><><><><><><><>");
+        box.getChildren().add(lTest);
+        Label lTest1 = new Label("Hallo Welt") ;
+        box.getChildren().add(lTest1);
+        Label lTest2 = new Label("Hier bin ich, lol") ;
+        box.getChildren().add(lTest2);
+        Label lTest3 = new Label("<><><><><><><><><>");
+        box.getChildren().add(lTest3);
 
         Scene scene = new Scene(box, 400,250);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Mein kleines sinnloses Fensterchen");
         primaryStage.show();
+
+        ;
     }
 }
