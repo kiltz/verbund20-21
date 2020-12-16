71232036ff74890fee91f134195903a328f43c73
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-16 10:21:56 +0100

    push - FibonacciFolge

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -1,23 +1,19 @@
 package basic.aufgabe3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FibonacciFolge {
     public static void main(String[] args) {
-        ArrayList<Integer> listOfNumbers = new ArrayList<>();
-        for (int i = 0;;i++) {
-            if (i == 0) {
-                listOfNumbers.add(0);
-            } else if (i == 1) {
-                listOfNumbers.add(1);
+        ArrayList<Integer> listOfNumbers = new ArrayList<>(Arrays.asList(0, 1));
+        for (int i = 2; ; i++) {
+            if (listOfNumbers.get(i - 1) + listOfNumbers.get(i - 2) >= 100) {
+                System.out.println(listOfNumbers);
+                break;
             } else {
-                if (listOfNumbers.get(i - 1) + listOfNumbers.get(i - 2) >= 100) {
-                    System.out.println(listOfNumbers);
-                    break;
-                } else {
-                    listOfNumbers.add(listOfNumbers.get(i - 1) + listOfNumbers.get(i - 2));
-                }
+                listOfNumbers.add(listOfNumbers.get(i - 1) + listOfNumbers.get(i - 2));
             }
         }
     }
 }
+
