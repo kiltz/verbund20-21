bcf3f8059454a3c5722b44d610e3cf31ee41ed28
AlexBleicher <alexander.bleicher@gmail.com>
2020-12-16 10:12:42 +0100

    Fibonacci ohne 144

--- src/main/java/oop/basic/aufgabeFibonacci/FibonacciFolge.java
+++ src/main/java/oop/basic/aufgabeFibonacci/FibonacciFolge.java
@@ -13,10 +13,13 @@ public class FibonacciFolge {
         long number2=1;
         long number3;
 
-        while(number2<=zahl){
+        while(number2<zahl){
             number3=number1+number2;
             number1=number2;
             number2=number3;
+            if(number2>=100){
+                break;
+            }
             System.out.println(number2);
         }
 
