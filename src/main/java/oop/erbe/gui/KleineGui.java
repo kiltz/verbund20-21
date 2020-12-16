cdb76e35a79bc4cf30374278d322668e02e32eba
pauleman1 <paul.hofmann@ingentis.com>
2020-12-16 14:55:30 +0100

    gui pauloo

--- src/main/java/oop/erbe/gui/KleineGui.java
+++ src/main/java/oop/erbe/gui/KleineGui.java
@@ -5,10 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -35,19 +33,8 @@ public class KleineGui extends Application {
         Label lStatus = new Label("Ein Status");
         box.getChildren().add(lStatus);
 
-        DropShadow shadow = new DropShadow();
-        button.addEventHandler(MouseEvent.MOUSE_ENTERED,
-                new EventHandler<MouseEvent>() {
-                    @Override public void handle(MouseEvent e) {
-                        button.setEffect(shadow);
-                    }
-                });
-        button.addEventHandler(MouseEvent.MOUSE_EXITED,
-                new EventHandler<MouseEvent>() {
-                    @Override public void handle(MouseEvent e) {
-                        button.setEffect(null);
-                    }
-                });
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Passwort");
 
         Scene scene = new Scene(box, 400,250);
         primaryStage.setScene(scene);
