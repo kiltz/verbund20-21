45bf65564474988ed7729655b5a2167d984e90c2
Fabian <fab.lechner@gmail.com>
2020-12-16 10:31:01 +0100

    Aufgabe Fibonacci

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -8,7 +8,7 @@ public class FibonacciFolge {
         System.out.println(zahl1);
         System.out.println(zahl2);
         System.out.println(zahl3);
-        while ( zahl3 < 100) {
+        while ( zahl3 + zahl2 < 100) {
             zahl1 = zahl2;
             zahl2 = zahl3;
             zahl3 = zahl1 + zahl2;
