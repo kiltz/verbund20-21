32ce971f20e8c6c6a749445d19b2c68de45da066
FabianKasbeitzer <fabian2312.kasbeitzer@gmail.com>
2020-12-16 10:14:48 +0100

    test

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -3,5 +3,17 @@ package basic.aufgabe3;
 public class FibonacciFolge {
     public static void main(String[] args) {
         //Gebe die Fibonacci bis 100 aus
+        int erstezahl = 0;
+        int zweitezahl = 1;
+        int drittezahl = 0;
+        System.out.println(erstezahl);
+        System.out.println(zweitezahl);
+        while ( drittezahl <= 80) {
+            drittezahl = zweitezahl + erstezahl;
+            System.out.println(drittezahl);
+            erstezahl = zweitezahl;
+            zweitezahl = drittezahl;
+
+        }
     }
 }
