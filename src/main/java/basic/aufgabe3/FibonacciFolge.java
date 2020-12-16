aa9845b5a5a5534f1b3a8bff442aa0365d9d3127
Fabian <fab.lechner@gmail.com>
2020-12-16 10:27:08 +0100

    Fabian Lechner

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -2,14 +2,17 @@ package basic.aufgabe3;
 
 public class FibonacciFolge {
     public static void main(String[] args) {
-        int zahl = 0;
-        int zahl1 = 1;
-        int zahl2 = 0;
-        while ( zahl2 < 100) {
-            zahl2 =zahl + zahl1;
-            zahl = zahl1;
+        int zahl1 = 0;
+        int zahl2 = 1;
+        int zahl3 = zahl1 + zahl2;
+        System.out.println(zahl1);
+        System.out.println(zahl2);
+        System.out.println(zahl3);
+        while ( zahl3 < 100) {
             zahl1 = zahl2;
-            System.out.println(zahl2);
+            zahl2 = zahl3;
+            zahl3 = zahl1 + zahl2;
+            System.out.println(zahl3);
         }
     }
 }
