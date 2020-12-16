ae0da2da882a38e3593470b33a5eecd5849ff489
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-16 14:37:47 +0100

    push - KleineGui

--- src/main/java/oop/erbe/gui/KleineGui.java
+++ src/main/java/oop/erbe/gui/KleineGui.java
@@ -3,6 +3,7 @@ package oop.erbe.gui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -23,6 +24,10 @@ public class KleineGui extends Application {
         box.getChildren().add(lStatus);
         Label lStatus2 = new Label("Hans möchte schlafen.");
         box.getChildren().add(lStatus2);
+        TextField textFeld = new TextField("Hans versucht herauszufinden was ein Textfeld ist."+
+                " Hans hat herausgefunden was ein Textfeld ist und hat es deaktiviert");
+        textFeld.setEditable(false);
+        box.getChildren().add(textFeld);
 
         Scene scene = new Scene(box, 400,250);
         primaryStage.setScene(scene);
