8b073a7f3b4c0aae633fee7d6903c83a90593aca
PatrikLeg <PatrikLeggeri@web.de>
2020-12-16 10:53:43 +0100

    Jetzt aber

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -2,6 +2,19 @@ package basic.aufgabe3;
 
 public class FibonacciFolge {
     public static void main(String[] args) {
-        //Gebe die Fibonacci bis 100 aus
+        int zahl1 = 0;
+        int zahl2 = 1;
+        int zahl3;
+
+        System.out.print(zahl1 + " " + zahl2);
+
+        for(int i = 0;i < 100; i++){
+            zahl3 = zahl1 + zahl2;
+            if (zahl3 <= 100) {
+                System.out.print(" " + zahl3);
+                zahl1 = zahl2;
+                zahl2 = zahl3;
+            }
+        }
     }
 }
