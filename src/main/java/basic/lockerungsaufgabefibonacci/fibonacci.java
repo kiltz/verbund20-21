ebb718ffbe4c3a67ea7e0f2d99ddd49ca767c617
pauleman1 <paul.hofmann@ingentis.com>
2020-12-16 10:08:24 +0100

    fibonacci neu paul

--- src/main/java/basic/lockerungsaufgabefibonacci/fibonacci.java
+++ src/main/java/basic/lockerungsaufgabefibonacci/fibonacci.java
@@ -3,10 +3,9 @@ package basic.lockerungsaufgabefibonacci;
 public class fibonacci {
     public static void main(String[] args) {
         int z1 = 0, z2 = 1, zahl;
-
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             zahl = z2 + z1;
-            if ( zahl <= 100 && zahl >= 0) {
+            if ( zahl <= 10 && zahl >= 0) {
                 System.out.println("Nummer: " + i + " - " + zahl);
             }
             z1 = z2;
@@ -14,5 +13,3 @@ public class fibonacci {
         }
     }
 }
-
-
