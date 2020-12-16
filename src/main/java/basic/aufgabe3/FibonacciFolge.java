2d920be63945f408e1b77ff752baf2ab655440e7
julianwgit <julianwonnenberg@gmail.com>
2020-12-16 10:31:51 +0100

    Fibonacci Folge überarbeitet

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -3,16 +3,19 @@ package basic.aufgabe3;
 public class FibonacciFolge {
     public static void main(String[] args) {
         //Gebe die Fibonacci bis 100 aus
-        int ersteZahl = 1;
-        int zweiteZahl = 0;
+        int ersteZahl = 0;
+        int zweiteZahl = 1;
+        int dritteZahl = ersteZahl + zweiteZahl;
 
         System.out.println("Fibonacci Folge");
 
-        while (ersteZahl < 50){
+        while (+dritteZahl <= 100){
             ersteZahl = ersteZahl + zweiteZahl;
             zweiteZahl = zweiteZahl + ersteZahl;
+            dritteZahl = ersteZahl + zweiteZahl;
             System.out.println(+ ersteZahl);
             System.out.println(+ zweiteZahl);
+            System.out.println(+ dritteZahl);
         }
     }
 }
