60fd2e0e1929ebd29503700b4239a071c5808ed6
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-16 09:40:59 +0100

    push - Konto-Test // 16-12-2020

--- src/main/java/oop/basic/aufgabe/Konto.java
+++ src/main/java/oop/basic/aufgabe/Konto.java
@@ -5,21 +5,26 @@ public class Konto {
     private int kontostand = 0;
     private int dispo = 0;
 
+
     public int getKontostand() {
         return this.kontostand;
     }
+
 
     public int getDispo() {
         return this.dispo;
     }
 
+
     public void setDispo(int zahl) {
         this.dispo += zahl;
     }
 
+
     public void einzahlen(int zahl) {
         this.kontostand += zahl;
     }
+
 
     public void auszahlen(int zahl) {
         if (this.kontostand - zahl >= -this.dispo) {
