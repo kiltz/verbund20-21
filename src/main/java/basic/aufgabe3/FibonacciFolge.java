6e435ad6f8a9ada131f35205c26f91799a62b0a7
Mikey <mikeybarbat@gmail.com>
2020-12-16 10:07:18 +0100

    Kleiner Fehler ist noch Enthalten

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -3,5 +3,17 @@ package basic.aufgabe3;
 public class FibonacciFolge {
     public static void main(String[] args) {
         //Gebe die Fibonacci bis 100 aus
-    }
+
+            int zahl1 =0;
+            int zahl2 =1;
+            int zahl3 =0;
+        System.out.print(zahl1+" "+zahl2);
+            while(zahl3<100)
+            {
+                zahl3=zahl1+zahl2;
+                System.out.print(" "+zahl3);
+                zahl1=zahl2;
+                zahl2=zahl3;
+            }
+        }
 }
