e8b0c44bf3f2b4d64e077555560b809f04dd1b14
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-16 15:06:39 +0100

    push - KleineGui

--- src/main/java/oop/erbe/gui/KleineGui.java
+++ src/main/java/oop/erbe/gui/KleineGui.java
@@ -6,6 +6,12 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+import java.awt.*;
+import java.io.FileInputStream;
 
 public class KleineGui extends Application {
 
@@ -21,6 +27,12 @@ public class KleineGui extends Application {
         box.getChildren().add(lInfo);
         Label lStatus = new Label("Ein Status");
         box.getChildren().add(lStatus);
+
+        Image image = new Image("https://hans-wagner.de/wp-content/uploads/2017/02/Hans_Wagner_Logo_rgb.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(400);
+        imageView.setPreserveRatio(true);
+        box.getChildren().add(imageView);
 
         Scene scene = new Scene(box, 400,250);
         primaryStage.setScene(scene);
