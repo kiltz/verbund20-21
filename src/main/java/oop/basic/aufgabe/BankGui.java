8af7d1206db3377d095491d5be56d13f8810a726
FabianKasbeitzer <fabian2312.kasbeitzer@gmail.com>
2020-12-17 11:15:52 +0100

    test

--- src/main/java/oop/basic/aufgabe/BankGui.java
+++ src/main/java/oop/basic/aufgabe/BankGui.java
@@ -7,7 +7,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+import oop.basic.aufgabe.muloe.Konto;
 import javax.xml.soap.Text;
 
 public class BankGui extends Application {
@@ -23,22 +23,22 @@ public class BankGui extends Application {
         hBoxName.getChildren().addAll(tfBetrag);
         // 2. Erzeuge einen Button für "setze Dispo"
             // er soll eine Methode setDispo(...) aufrufen
-        Button bLogin = new Button("Setze Dispo");
-        bLogin.setOnAction(k -> getDispo(tfBetrag));
+        Button bDispo = new Button("Setze Dispo");
         // 3. erzeuge einen Button "einzahlen"
             // er soll eine Methode einzahlen(...) aufrufen
         Button bEinzahlen = new Button("Einzahlen");
-        bLogin.setOnAction(k -> einzahlen(bEinzahlen));
         // 4. erzeuge einen Button "auszahlen"
             // er soll eine Methode auszahlen(...) aufrufen
+        Button bAuszahlen = new Button("Auszahlen");
 
         // 5. Überprüfe, das die Button die richtigen Methoden aufrufen.
-
+        bDispo.setOnAction(k -> getDispo(tfBetrag));
+        bAuszahlen.setOnAction(k -> auszahlen(bAuszahlen));
+        bEinzahlen.setOnAction(k -> einzahlen(bEinzahlen));
         // 6. Wandele den Betrag aus dem TextFeld in eine Zahl um
+        int zahl = Integer.parseInt("tfBetrag");
         // Beispiel:
-        int zahl = Integer.parseInt("1234");
-        box.getChildren().addAll(hBoxName, bLogin);
-
+        box.getChildren().addAll(hBoxName, bDispo, bEinzahlen, bAuszahlen);
         Scene scene = new Scene(box, 400, 250);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Bank");
