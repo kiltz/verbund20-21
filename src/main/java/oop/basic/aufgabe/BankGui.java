de6a729efb7d8611f62813c02ca777bf560333fc
Fabian <fab.lechner@gmail.com>
2020-12-17 12:53:36 +0100

    Aufgabe BankGui

--- src/main/java/oop/basic/aufgabe/BankGui.java
+++ src/main/java/oop/basic/aufgabe/BankGui.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,13 +12,19 @@ import javafx.stage.Stage;
 public class BankGui extends Application {
 
     private TextField  tfBetrag;
+    private Konto konto;
+    private Label lKontostand;
+    private Label lDispo;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         VBox box = new VBox(10);
 
+        konto = new Konto();
         tfBetrag = new TextField();
+        lKontostand = new Label();
+        lDispo = new Label();
 
         Button bsetzeDispo = new Button("setze Dispo");
         bsetzeDispo.setOnAction(e -> setDispo(e) );
@@ -28,7 +35,11 @@ public class BankGui extends Application {
         Button bAuszahlen = new Button("auszahlen");
         bAuszahlen.setOnAction(e -> auszahlen(e));
 
-        box.getChildren().addAll(tfBetrag,bsetzeDispo, bEinzahlen,bAuszahlen);
+        lKontostand.setText("Kontostand: " + konto.getKontostand());
+        lDispo.setText("Dispo: " + konto.getDispo());
+
+
+        box.getChildren().addAll(tfBetrag,bsetzeDispo, bEinzahlen,bAuszahlen,lKontostand, lDispo);
 
 
         Scene scene = new Scene(box, 400, 250);
@@ -39,20 +50,21 @@ public class BankGui extends Application {
     }
 
     private void setDispo(ActionEvent e) {
-        System.out.println("Setze Dispo");
         int betrag = Integer.parseInt(tfBetrag.getText());
-        System.out.println(betrag);
+        konto.setDispo(betrag);
+        lDispo.setText("Dispo: " + konto.getDispo());
+
     }
 
     private void einzahlen(ActionEvent e) {
-        System.out.println("Betrag einzahlen");
         int betrag = Integer.parseInt(tfBetrag.getText());
-        System.out.println(betrag);
+        konto.einzahlen(betrag);
+        lKontostand.setText("Kontostand: " + konto.getKontostand());
     }
 
     private void auszahlen(ActionEvent e) {
-        System.out.println("Betrag auszahlen");
         int betrag = Integer.parseInt(tfBetrag.getText());
-        System.out.println(betrag);
+        konto.auszahlen(betrag);
+        lKontostand.setText("Kontostand: " + konto.getKontostand());
     }
 }
