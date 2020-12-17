2c7524763c36bd57c6c4ee53483a6d526d8e5f4b
Fabian <fab.lechner@gmail.com>
2020-12-17 13:45:14 +0100

    Aufgabe BankGUI

--- src/main/java/oop/basic/aufgabe/BankGui.java
+++ src/main/java/oop/basic/aufgabe/BankGui.java
@@ -2,6 +2,7 @@ package oop.basic.aufgabe;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,6 +21,7 @@ public class BankGui extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         VBox box = new VBox(10);
+        box.setPadding(new Insets(10, 20, 20, 20));
 
         konto = new Konto();
         tfBetrag = new TextField();
