59e1e1edddb8ceeace0bf59603061363da48615e
Leon Wolf <leon.wolf.leon@hotmail.com>
2020-12-17 14:49:57 +0100

    push - KreisGui // Mit Farbe und so

--- src/main/java/oop/basic/KreisGui.java
+++ src/main/java/oop/basic/KreisGui.java
@@ -8,8 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
-
 
 
 public class KreisGui extends Application {
@@ -17,28 +17,30 @@ public class KreisGui extends Application {
 
     private TextField tfEingabe;
     private Label lResult;
-    
+
     Kreis kreis = new Kreis();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         VBox box = new VBox(10);
         HBox hBoxEingabe = new HBox(10);
-        
+        HBox hBoxHeader = new HBox(10);
+
         tfEingabe = new TextField();
         lResult = new Label("Umfang: ");
+        Label lRadius = new Label("Radius hier eingeben: ");
 
         Button calculate = new Button("Berechne!");
         calculate.setOnAction(e -> berechneUmfang(e));
 
-        hBoxEingabe.getChildren().addAll(tfEingabe, calculate);
-        box.getChildren().addAll(hBoxEingabe, lResult);
+        hBoxHeader.getChildren().add(lRadius);
+        hBoxEingabe.getChildren().addAll( tfEingabe, calculate);
+        box.getChildren().addAll(hBoxHeader, hBoxEingabe, lResult);
 
         Scene scene = new Scene(box, 400, 250);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Kreis-Umfang");
         primaryStage.show();
-
     }
 
     private void berechneUmfang(ActionEvent e) {
@@ -46,10 +48,15 @@ public class KreisGui extends Application {
             int value = Integer.parseInt(tfEingabe.getText());
             kreis.setRadius(value);
             lResult.setText("Umfang: " + kreis.berechneUmfang());
+            lResult.setTextFill(Paint.valueOf("#000000"));
+
         } catch (NumberFormatException f) {
-            lResult.setText("Ungueltiger Wert eingegeben!");
+            lResult.setText("Ungueltiger Wert eingegeben! " + f.getMessage());
+            lResult.setTextFill(Paint.valueOf("#ff0000"));
+
         } catch (Exception exception) {
             lResult.setText("Dein Radius ist zu klein!");
+            lResult.setTextFill(Paint.valueOf("#ff0000"));
         }
     }
 }
