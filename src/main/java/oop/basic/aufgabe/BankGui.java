fb01d7824db8e1c4acdbd23a44b53dfc77bda85a
Fabian <fab.lechner@gmail.com>
2020-12-17 11:19:36 +0100

    Aufgabe BankGui

--- src/main/java/oop/basic/aufgabe/BankGui.java
+++ src/main/java/oop/basic/aufgabe/BankGui.java
@@ -19,32 +19,21 @@ public class BankGui extends Application {
 
         tfBetrag = new TextField();
 
-        // 2. Erzeuge einen Button für "setze Dispo"
-            // er soll eine Methode setDispo(...) aufrufen
-
         Button bsetzeDispo = new Button("setze Dispo");
         bsetzeDispo.setOnAction(e -> setDispo(e) );
 
-        // 3. erzeuge einen Button "einzahlen"
-            // er soll eine Methode einzahlen(...) aufrufen
-
         Button bEinzahlen = new Button("einzahlen");
         bEinzahlen.setOnAction(e -> einzahlen(e));
-
-        // 4. erzeuge einen Button "auszahlen"
-            // er soll eine Methode auszahlen(...) aufrufen
 
         Button bAuszahlen = new Button("auszahlen");
         bAuszahlen.setOnAction(e -> auszahlen(e));
 
         box.getChildren().addAll(tfBetrag,bsetzeDispo, bEinzahlen,bAuszahlen);
 
-        // 5. Überprüfe, das die Button die richtigen Methoden aufrufen.
-
         // 6. Wandele den Betrag aus dem TextFeld in eine Zahl um
         // Beispiel:
 
-        int zahl = Integer.parseInt("1234");
+        int betrag = Integer.parseInt("1234");
 
 
         Scene scene = new Scene(box, 400, 250);
