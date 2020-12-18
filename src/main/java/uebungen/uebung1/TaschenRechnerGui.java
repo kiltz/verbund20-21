23ea8861ec4ab039e838894311a05ca055d9353e
pauleman1 <paul.hofmann@ingentis.com>
2020-12-18 10:32:11 +0100

    fertigenu

--- src/main/java/uebungen/uebung1/TaschenRechnerGui.java
+++ src/main/java/uebungen/uebung1/TaschenRechnerGui.java
@@ -27,10 +27,8 @@ public class TaschenRechnerGui extends javafx.application.Application
 
         box.setTop(new Label("Trage eine Zahl ein: "));
 
-
         Label lblAddition = new Label("");
         box.setBottom(lblAddition);
-
 
         GridPane center = new GridPane();
         center.setVgap(5);
@@ -40,16 +38,10 @@ public class TaschenRechnerGui extends javafx.application.Application
         TextField zahl2 = new TextField("0");
         zahl2.setPrefWidth(150);
 
-
-
         center.add(new Label("Zahl1"), 0, 0);
         center.add(new Label("+"), 0, 0);
         Button rechne = new Button("=");
         Label nez = new Label("+");
-
-
-
-
 
         center.add(new Label("Zahl2"), 0, 2);
         center.add(zahl1, 1, 0);
