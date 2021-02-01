187a087e9c357e07ced77ac666c11319fa70a3f5
Fabian <fab.lechner@gmail.com>
2021-02-01 10:23:58 +0100

    RechnerApp

---
+++ src/main/java/gui/RechnerApp.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class RechnerApp extends Application {
+
+    public static void main(String[] args) {
+        launch(null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        TextField tf1 = new TextField("Tf1");
+        TextField tf2 = new TextField("Tf2");
+        Label l = new Label(" + ");
+        Label r = new Label (" = ");
+        HBox root = new HBox();
+        Button e = new Button("Ergebnis");
+        root.getChildren().addAll(tf1,l,tf2,r,e);
+        Scene scene = new Scene(root, 500, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
