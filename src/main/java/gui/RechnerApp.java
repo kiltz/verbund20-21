c54d6a1ceb1bf6fa026e596fab49fa56f5277d79
Mikey <mikeybarbat@gmail.com>
2021-02-01 10:21:01 +0100

    RechnerApp v1

---
+++ src/main/java/gui/RechnerApp.java
@@ -0,0 +1,37 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ * Aufgabe:
+ * Kopiert diese Datei nach RechnerApp.
+ * Zwei Textfelder, zwei Label und einen Button anzeigt.
+ *
+ * [.5.] + [.2.] (=) 7
+ */
+public class RechnerApp extends Application {
+
+    public static void main(String[] args) {
+        launch(null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label l1 = new Label("+");
+        Label l2 = new Label("7");
+        TextField t1 = new TextField();
+        TextField t2 = new TextField();
+        Button b1 = new Button("=");
+        HBox root = new HBox();
+        root.getChildren().addAll(t1, l1, t2, b1,l2);
+        Scene scene = new Scene(root, 300, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
