5c67f2e04c09e6f632ebd61db6d9cda35ddf897c
Miriam <mes.gleich@gmail.com>
2021-02-01 14:20:46 +0100

    Login

--- src/main/java/gui/uebung/LoginApp.java
+++ src/main/java/gui/uebung/LoginApp.java
@@ -1,10 +1,15 @@
 package gui.uebung;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 /**
  * Aufgabe:
@@ -25,17 +30,67 @@ import javafx.stage.Stage;
  */
 public class LoginApp extends Application {
 
+
+    private TextField password;
+    private TextField username;
+    private Label llogin;
+
     public static void main(String[] args) {
         launch(null);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label l = new Label("Hallo App!");
-        HBox root = new HBox();
-        root.getChildren().add(l);
-        Scene scene = new Scene(root, 300, 400);
+
+        username = new TextField();
+        username.setMaxWidth(150);
+        password = new TextField();
+        password.setMaxWidth(150);
+        Label lusername = new Label("Username: ");
+        Label lpassword = new Label("Password:  ");
+        Button blogin = new Button("Login");
+        blogin.setOnAction(e-> login(e));
+        llogin = new Label ();
+
+
+
+
+        VBox root = new VBox();
+
+        HBox zeile1 = new HBox();
+        zeile1.getChildren().add(lusername);
+        zeile1.getChildren().add(username);
+
+        HBox zeile2 = new HBox();
+        zeile2.getChildren().add(lpassword);
+        zeile2.getChildren().add(password);
+
+        HBox zeile3 = new HBox();
+        zeile3.getChildren().add(blogin);
+
+        HBox zeile4 = new HBox();
+        zeile4.getChildren().add(llogin);
+
+
+        root.getChildren().add(zeile1);
+        root.getChildren().add(zeile2);
+        root.getChildren().add(zeile3);
+        root.getChildren().add(zeile4);
+
+
+
+        Insets insets = new Insets(50, 50, 10, 50);
+        root.setPadding(insets);
+        Scene scene = new Scene(root, 500, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void login(ActionEvent e) {
+        if (username.getText().equals("User") && password.getText().equals("abc123")) {
+            llogin.setText("Erfolgreicher Login");
+        } else {
+            llogin.setText("Falsches Passwort oder Nutzername");
+        }
     }
 }
