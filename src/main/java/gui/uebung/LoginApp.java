2954c953c820e56d1e30614bc3cafdf60a25f000
Fabian Kasbeitzer <fabian2312.kasbeitzer@gmail.com>
2021-02-01 15:06:15 +0100

    Aufgabe

--- src/main/java/gui/uebung/LoginApp.java
+++ src/main/java/gui/uebung/LoginApp.java
@@ -1,41 +1,79 @@
 package gui.uebung;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-/**
- * Aufgabe:
- * Zeigt zwei Eingabefelder für Benutzername und Passwort
- * Beschribt die Eingabefelder mit einem Label
- *
- * Erzeugt einen Button (Login)
- *
- * Macht den Dialog hübsch
- *
- * Validiert den Dialog
- * 1. Benutzername darf nicht leer sein
- * 2. Passwort darf nicht leer sein
- * 3. Benutzernamen und Passwort müssen irgendwelchen festen Werten entsprechen
- *
- * Hinweis: Schachtelt VBox und HBox
- *
- */
-public class LoginApp extends Application {
+public class LoginApp extends Application{
+    private TextField tfName;
+    private PasswordField tfPasswort;
+    private String Passwort = "12345";
+    private String Name = "Test";
+    private Label output;
+
+
+    private VBox box;
 
     public static void main(String[] args) {
-        launch(null);
+        launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label l = new Label("Hallo App!");
-        HBox root = new HBox();
-        root.getChildren().add(l);
-        Scene scene = new Scene(root, 300, 400);
+
+        box = new VBox(10);
+        box.setPadding(new Insets(10, 20, 20, 20));
+
+        HBox hBoxName = new HBox(10);
+        Label lName = new Label("Name");
+        tfName = new TextField();
+        output = new Label();
+
+        hBoxName.getChildren().addAll(lName, tfName);
+
+        HBox hBoxPasswd = new HBox(10);
+        Label lPasswort = new Label("Passwort");
+
+        tfPasswort = new PasswordField();
+        hBoxPasswd.getChildren().addAll(lPasswort, tfPasswort);
+
+        Button bLogin = new Button("Einloggen");
+        bLogin.setOnAction(e -> login(e));
+
+        box.getChildren().addAll(hBoxName, hBoxPasswd, bLogin, output);
+
+        Scene scene = new Scene(box, 400, 250);
         primaryStage.setScene(scene);
+
+
+        primaryStage.setTitle("Login");
         primaryStage.show();
+
+    }
+
+    private void login(ActionEvent e) {
+        String name = tfName.getText();
+        String passwort = tfPasswort.getText();
+        String blau = "#005091";
+        String rot = "#E23130";
+        if (Passwort.equals(passwort)&&Name.equals(name)) {
+            output.setText("Login erfolgreich");
+            output.setTextFill(Color.web(blau));
+        } else {
+            output.setText("Login nicht erfolgreich");
+            output.setTextFill(Color.web(rot));
+        }
+
+
     }
 }
