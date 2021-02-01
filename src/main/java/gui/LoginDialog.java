47af78abff639d7a6b19a9e43f88087bee609ec0
julianwgit <julianwonnenberg@gmail.com>
2021-02-01 15:00:32 +0100

    Taschenrechner mit Additionfunktion

--- src/main/java/gui/LoginDialog.java
+++ src/main/java/gui/LoginDialog.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
 public class LoginDialog extends Application{
     public static void main(String[] args) {
@@ -22,13 +23,19 @@ public class LoginDialog extends Application{
     private PasswordField password;
     private String loginId = "Julian";
     private String loginPw = "Passwort";
+    private Label lprufen;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        VBox box = new VBox(10);
+        box.setPadding(new Insets(10));
+
+        HBox hBoxName = new HBox(10);
         Label lname = new Label("Benutzername: ");
         tfName = new TextField();
 
+        HBox hBoxPassword = new HBox(10);
         Label lpw = new Label("Passwort: ");
         password = new PasswordField();
 
@@ -37,11 +44,30 @@ public class LoginDialog extends Application{
         Insets insets = new Insets(20);
         btnLogin.setOnAction(event -> login(e));
 
+        hBoxName.getChildren().addAll(lname, tfName);
+        hBoxPassword.getChildren().addAll(password, lpw);
+
+        Label lprufen = new Label("");
+
+        box.getChildren().addAll(hBoxName, hBoxPassword, btnLogin, lprufen);
+
     }
 
     private void login(ActionEvent e) {
         String name = tfName.getText();
         String passwort = password.getText();
         Label ausgabe = new Label();
+
+            if ("".equals(name) || "".equals(passwort)){
+            lprufen.setText("Es muss etwas eingegeben werden!");
+            lprufen.setTextFill(Color.RED);
+        }
+            else if("Test".equals(name) && "Geheim".equals(passwort)) {
+            lprufen.setText("Login erfolgreich!");
+            lprufen.setTextFill(Color.BLUE);
+        }else{
+            lprufen.setText("Login nicht erfolgreich!");
+            lprufen.setTextFill(Color.RED);
+        }
     }
 }
