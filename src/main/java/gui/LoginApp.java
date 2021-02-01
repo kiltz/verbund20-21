03485c0b357b94134f3529cce6bc3611459fd143
Fabian <fab.lechner@gmail.com>
2021-02-01 14:53:53 +0100

    LoginApp

--- src/main/java/gui/LoginApp.java
+++ src/main/java/gui/LoginApp.java
@@ -3,6 +3,8 @@ package gui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,63 +17,73 @@ import javafx.stage.Stage;
 public class LoginApp extends Application {
 
     private TextField tfName;
-    private VBox box;
     private PasswordField tfPasswort;
-    private Label ausgabe = new Label();
+    private Label ausgabe;
     private String loginName = "Hans";
     private String loginPasswort = "Geheim";
 
-    public LoginApp() {
-    }
-
     public static void main(String[] args) {
-        launch(args);
+        launch(null);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        box = new VBox(10.0);
-        box.setPadding(new Insets(10, 20, 20, 10));
-
-        HBox hBoxName = new HBox(10);
-        Label lBenutzername = new Label("Benutzername: ");
-        lBenutzername.setPrefSize(100.0,10.0);
-        tfName = new TextField();
-
-        hBoxName.getChildren().addAll(lBenutzername, tfName);
-
-        HBox hBoxPasswd = new HBox(10);
-        Label lPasswort = new Label("Passwort: ");
-        lPasswort.setPrefSize(100.0,10.0);
-        tfPasswort = new PasswordField();
-        hBoxPasswd.getChildren().addAll(lPasswort, tfPasswort);
-
-        Button bLogin = new Button("Login");
-        Insets insets = new Insets(10,10,10,10);
-        bLogin.setPadding(insets);
-        bLogin.setOnAction(e -> login(e));
-
-        box.getChildren().addAll(hBoxName, hBoxPasswd, bLogin);
-        
-        Scene scene = new Scene(box, 400, 200);
+        VBox root = new VBox(10.0);
+        root.setPadding(new Insets(10, 20, 20, 10));
+        root.getChildren().addAll(getBenutzerZeile(), getPasswortZeile(), getButtonZeile());
+        Scene scene = new Scene(root, 400, 200);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Login");
         primaryStage.show();
 
     }
+
+    private Node getBenutzerZeile() {
+        HBox box = new HBox(15);
+        box.setPadding(new Insets(10));
+        tfName = new TextField();
+        box.getChildren().addAll(new Label("Benutzername"), tfName);
+        return box;
+    }
+
+    private Node getPasswortZeile() {
+        HBox box = new HBox(15);
+        box.setPadding(new Insets(10));
+        tfPasswort = new PasswordField();
+        box.getChildren().addAll(new Label("Passwort"), tfPasswort);
+        return box;
+    }
+
+    private Node getButtonZeile() {
+        HBox box = new HBox(15);
+        box.setPadding(new Insets(10));
+        box.setAlignment(Pos.CENTER_RIGHT);
+        ausgabe = new Label();
+        Button bLogin = new Button("Login");
+        bLogin.setDefaultButton(true);
+        bLogin.setOnAction(e -> login(e));
+        box.getChildren().addAll(ausgabe, bLogin);
+        return box;
+    }
+
+
     private void login(ActionEvent e) {
         String name = tfName.getText();
         String passwort = tfPasswort.getText();
-        //Label ausgabe;
 
-            if (loginName.equals(name) && loginPasswort.equals(passwort)) {
-                //ausgabe = new Label("Login erfolgreich!");
-                ausgabe.setText("Login erfolgreich!");
-            } else {
-                //ausgabe = new Label("Login nicht erfolgreich!");
-                ausgabe.setText("Login nicht erfolgreich!");
-            }
-        box.getChildren().add(ausgabe);
+        if (loginName.equals(name) && loginPasswort.equals(passwort)) {
+            ausgabe = new Label("Login erfolgreich!");
+            ausgabe.setText("Login erfolgreich!");
+        } else if (loginName.equals(name) & !loginPasswort.equals(passwort)) {
+            ausgabe = new Label("Login nicht erfolgreich! Passwort falsch");
+            ausgabe.setText("Login nicht erfolgreich! Passwort falsch");
+        } else if (loginPasswort.equals(passwort) & !loginName.equals(name)) {
+            ausgabe = new Label("Login nicht erfolgreich! Benutzer unbekannt");
+            ausgabe.setText("Login nicht erfolgreich! Benutzer unbekannt");
+        } else {
+            ausgabe = new Label("Login nicht erfolgreich!");
+            ausgabe.setText("Login nicht erfolgreich!");
         }
     }
+}
