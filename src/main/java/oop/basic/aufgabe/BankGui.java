f347310ceb913831edbe1829560cd38f365c75d3
Fabian Kasbeitzer <fabian2312.kasbeitzer@gmail.com>
2021-02-01 09:14:15 +0100

    test

--- src/main/java/oop/basic/aufgabe/BankGui.java
+++ src/main/java/oop/basic/aufgabe/BankGui.java
@@ -16,7 +16,6 @@ public class BankGui extends Application {
     private Konto konto;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        konto = new Konto(getKontostand);
         VBox box = new VBox(10);
 
         // 1. erzeuge ein Eingabfeld für den Betrag
@@ -26,22 +25,17 @@ public class BankGui extends Application {
         // 2. Erzeuge einen Button für "setze Dispo"
             // er soll eine Methode setDispo(...) aufrufen
         Button bDispo = new Button("Setze Dispo");
-        bDispo.setOnAction(e -> getDispo(tfBetrag));
         // 3. erzeuge einen Button "einzahlen"
             // er soll eine Methode einzahlen(...) aufrufen
         Button bEinzahlen = new Button("Einzahlen");
-        bEinzahlen.setOnAction(e -> einzahlen(bEinzahlen));
         // 4. erzeuge einen Button "auszahlen"
             // er soll eine Methode auszahlen(...) aufrufen
-        Button bAuszahlen = new Button("Auszahlen");
-        bAuszahlen.setOnAction(e -> auszahlen(bAuszahlen));
         // 5. Überprüfe, das die Button die richtigen Methoden aufrufen.
         // 6. Wandele den Betrag aus dem TextFeld in eine Zahl um
         int zahl = Integer.parseInt("tfBetrag");
         Label lKontostand = new Label("Kontostand:" + konto);
         Label lAusgezahlt = new Label("Ausgezahlt");
         // Beispiel:
-        box.getChildren().addAll(hBoxName, bDispo, bEinzahlen, bAuszahlen, lKontostand, lAusgezahlt);
         Scene scene = new Scene(box, 400, 250);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Bank");
