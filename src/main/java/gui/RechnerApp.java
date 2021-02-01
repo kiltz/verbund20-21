b139a6066a355037651cb7d0a6c56fa4b51d9b68
Fabian Kasbeitzer <fabian2312.kasbeitzer@gmail.com>
2021-02-01 13:32:30 +0100

    Aufgabe

--- src/main/java/gui/RechnerApp.java
+++ src/main/java/gui/RechnerApp.java
@@ -1,6 +1,8 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,10 +14,14 @@ import javafx.stage.Stage;
  * Aufgabe:
  * Kopiert diese Datei nach RechnerApp.
  * Zwei Textfelder, zwei Label und einen Button anzeigt.
- *
+ * <p>
  * [.5.] + [.2.] (=) 7
  */
 public class RechnerApp extends Application {
+
+    private Label label2;
+    private TextField textfiel1;
+    private TextField textfield2;
 
     public static void main(String[] args) {
         launch(null);
@@ -23,20 +29,35 @@ public class RechnerApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label label1 = new Label("+");
-        Label label2 = new Label("7");
-        TextField texfield1 = new TextField("5");
-        TextField texfield2 = new TextField("2");
-        Button button1 = new Button("=");
-        HBox root = new HBox();
-        root.getChildren().add(texfield1);
-        root.getChildren().add(label1);
-        root.getChildren().add(texfield2);
-        root.getChildren().add(button1);
-        root.getChildren().add(label2);
-        Scene scene = new Scene(root, 500, 400);
+        Label lEins = new Label("+");
+        label2 = new Label("7");
+        textfiel1 = new TextField("5");
+        textfiel1.setMaxWidth(50);
+        textfield2 = new TextField("2");
+        textfield2.setMaxWidth(50);
+        Button bRechne = new Button(" = ");
+        bRechne.setDefaultButton(true);
+        bRechne.setOnAction(e -> rechne(e));
 
+        HBox root = new HBox();
+        root.setSpacing(10.0);
+        Insets insets = new Insets(20, 10, 10, 50);
+        root.setPadding(insets);
+        root.getChildren().addAll(textfiel1, lEins,textfield2, bRechne, label2);
+        Scene scene = new Scene(root, 400, 200);
+        primaryStage.setTitle("Rechner");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+    }
+
+    private void rechne(ActionEvent e) {
+        String eingabe1 = textfiel1.getText();
+        String eingabe2 = textfield2.getText();
+        int zahl1 = Integer.parseInt(eingabe1);
+        int zahl2 = Integer.parseInt(eingabe2);
+        int ergebnis = zahl1 + zahl2;
+        label2.setText(""+ergebnis);
     }
 }
