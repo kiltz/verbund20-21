2ea9d047085d55eefae8b4de8fa16e14745f3c5b
pauleman1 <paul.hofmann@ingentis.com>
2021-02-01 11:32:42 +0100

    PaulRechnerAppGui mit ausrechnen

--- src/main/java/gui/RechnerApp.java
+++ src/main/java/gui/RechnerApp.java
@@ -1,6 +1,8 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,32 +11,49 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class RechnerApp extends Application {
-
+    private TextField text1;
+    private TextField text2;
+    private Label ergebnis;
     public static void main(String[] args) {
         launch(null);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label l = new Label("7");
-        TextField eingabe5 = new TextField();
-        Label p = new Label("+");
-        TextField eingabe2 = new TextField();
-        Button button2 = new Button("=");
-
-
-
+        Label l1 = new Label("+");
+        text1=new TextField();
+        text2=new TextField();
+        Button button=new Button("=");
+        button.setOnAction(e->rechne(e));
+        ergebnis=new Label("");
         HBox root = new HBox();
-        root.getChildren().add(eingabe5);
-        root.getChildren().add(p);
-        root.getChildren().add(eingabe2);
-        root.getChildren().add(button2);
-        root.getChildren().add(l);
-
-
-
-        Scene scene = new Scene(root, 1200, 600);
+        root.setSpacing(10);
+        Insets insets= new Insets(10,10,10,10);
+        root.setPadding(insets);
+        root.getChildren().add(text1);
+        root.getChildren().add(l1);
+        root.getChildren().add(text2);
+        root.getChildren().add(button);
+        root.getChildren().add(ergebnis);
+        Scene scene = new Scene(root, 500, 100);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    public void rechne(ActionEvent e){
+        String t1=text1.getText();
+        String t2=text2.getText();
+        try{
+
+            int zahl1=Integer.parseInt(t1);
+            int zahl2=Integer.parseInt(t2);
+            int endwert=zahl1+zahl2;
+            endwert= (int) (Math.round((zahl1+zahl2)*100)/100.0);
+            ergebnis.setText("Ihr Ergebnis ist: " + endwert);
+        }
+        catch(NumberFormatException f){
+            ergebnis.setText("Fehlerhafte Eingabe!");
+        }
+    }
 }
+
