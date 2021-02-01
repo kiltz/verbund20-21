3d60410adac43a294b5520807183e685399e27b4
Friedrich Kiltz <f@kiltz.de>
2021-02-01 11:11:03 +0100

    Dialog hübsch gemacht

--- src/main/java/gui/aufgabe/mueloe/RechnerApp.java
+++ src/main/java/gui/aufgabe/mueloe/RechnerApp.java
@@ -1,6 +1,7 @@
 package gui.aufgabe.mueloe;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -26,17 +27,23 @@ public class RechnerApp extends Application {
         Label lEins = new Label("+");
         Label lErgebnis = new Label("7");
         TextField tfEins = new TextField("5");
+        tfEins.setMaxWidth(50);
         TextField tfZwei = new TextField("2");
+        tfZwei.setMaxWidth(50);
         Button bRechne = new Button(" = ");
-
+        bRechne.setDefaultButton(true);
 
         HBox root = new HBox();
+        root.setSpacing(10.0);
+        Insets insets = new Insets(20, 10, 10, 50);
+        root.setPadding(insets);
         root.getChildren().add(tfEins);
         root.getChildren().add(lEins);
         root.getChildren().add(tfZwei);
         root.getChildren().add(bRechne);
         root.getChildren().add(lErgebnis);
-        Scene scene = new Scene(root, 300, 400);
+        Scene scene = new Scene(root, 400, 200);
+        primaryStage.setTitle("Rechner");
         primaryStage.setScene(scene);
         primaryStage.show();
 
