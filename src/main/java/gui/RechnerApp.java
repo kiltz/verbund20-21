4e677a8fee9907c9752f8cc1b293aa04fa56a578
Leon Wolf <leon.wolf.leon@hotmail.com>
2021-02-01 11:09:32 +0100

    push - RechnerApp // MaxWidth

--- src/main/java/gui/RechnerApp.java
+++ src/main/java/gui/RechnerApp.java
@@ -1,68 +1,71 @@
-    package gui;
+package gui;
 
-    import javafx.application.Application;
-    import javafx.event.ActionEvent;
-    import javafx.geometry.Insets;
-    import javafx.scene.Scene;
-    import javafx.scene.control.Button;
-    import javafx.scene.control.Label;
-    import javafx.scene.control.TextField;
-    import javafx.scene.layout.HBox;
-    import javafx.scene.paint.Color;
-    import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-    import javax.xml.soap.Text;
-    import java.io.IOException;
+/**
+ * Aufgabe:
+ * Kopiert diese Datei nach RechnerApp.
+ * Zwei Textfelder, zwei Label und einen Button anzeigt.
+ *
+ * [.5.] + [.2.] (=) 7
+ */
 
-    /**
-     * Aufgabe:
-     * Kopiert diese Datei nach RechnerApp.
-     * Zwei Textfelder, zwei Label und einen Button anzeigt.
-     * <p>
-     * [.5.] + [.2.] (=) 7
-     */
-    public class RechnerApp extends Application {
+public class RechnerApp extends Application {
 
-        private TextField tfZahl1;
-        private TextField tfZahl2;
-        private Label lErgebnis;
+    private TextField tfZahl1;
+    private TextField tfZahl2;
+    private Label lErgebnis;
 
-        public static void main(String[] args) {
-            launch();
-        }
+    public static void main(String[] args) {
+        launch();
+    }
 
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            tfZahl1 = new TextField();
-            Label plus = new Label("+");
-            tfZahl2 = new TextField();
-            lErgebnis = new Label("Ergebnis:");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        tfZahl1 = new TextField();
+        tfZahl1.setMaxWidth(80);
 
-            Button btBerechne = new Button("=");
-            btBerechne.setOnAction(e -> berechne(e));
+        Label plus = new Label("+");
 
-            HBox root = new HBox(10);
-            root.setPadding(new Insets(10, 20, 20, 20));
+        tfZahl2 = new TextField();
+        tfZahl2.setMaxWidth(80);
 
-            root.getChildren().addAll(tfZahl1, plus, tfZahl2, btBerechne, lErgebnis);
+        lErgebnis = new Label("Ergebnis:");
 
-            Scene scene = new Scene(root, 700, 250);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
+        Button btBerechne = new Button("=");
+        btBerechne.setOnAction(e -> berechne(e));
 
-        private void berechne(ActionEvent e) {
-            try {
-                int zahl1 = Integer.parseInt(tfZahl1.getText());
-                int zahl2 = Integer.parseInt(tfZahl2.getText());
-                int ergebnis = zahl1 + zahl2;
+        HBox root = new HBox(10);
+        root.setPadding(new Insets(10, 20, 20, 20));
 
-                lErgebnis.setText("Ergebnis: " + ergebnis);
-                lErgebnis.setTextFill(Color.valueOf("#000000"));
+        root.getChildren().addAll(tfZahl1, plus, tfZahl2, btBerechne, lErgebnis);
 
-            } catch (NumberFormatException f) {
-                lErgebnis.setText("Ungueltige Eingabe!");
-                lErgebnis.setTextFill(Color.valueOf("#ff0000"));
-            }
+        Scene scene = new Scene(root, 700, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void berechne(ActionEvent e) {
+        try {
+            int zahl1 = Integer.parseInt(tfZahl1.getText());
+            int zahl2 = Integer.parseInt(tfZahl2.getText());
+            int ergebnis = zahl1 + zahl2;
+
+            lErgebnis.setText("Ergebnis: " + ergebnis);
+            lErgebnis.setTextFill(Color.valueOf("#000000"));
+
+        } catch (NumberFormatException f) {
+            lErgebnis.setText("Ungueltige Eingabe!");
+            lErgebnis.setTextFill(Color.valueOf("#ff0000"));
         }
     }
+}
