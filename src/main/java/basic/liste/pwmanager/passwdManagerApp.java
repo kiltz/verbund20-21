02ed42879712284cedbc9e3d50647a343391687e
AlexBleicher <alexander.bleicher@gmail.com>
2021-02-02 11:35:45 +0100

    Aufgabe passwdManagerApp Eintragen

--- src/main/java/basic/liste/pwmanager/passwdManagerApp.java
+++ src/main/java/basic/liste/pwmanager/passwdManagerApp.java
@@ -1,15 +1,22 @@
 package basic.liste.pwmanager;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 public class passwdManagerApp extends Application {
     public TextField tfName;
+    public TextField tfBenutzer;
+    public PasswordField pfPassword;
     public static void main(String[] args) {
         launch(args);
     }
@@ -18,8 +25,12 @@ public class passwdManagerApp extends Application {
         VBox root = new VBox();
 
         HBox boxLineName=getLineName();
+        HBox boxLineBenutzer=getLineBenutzer();
+        HBox boxLinePassword=getLinePassword();
 
         root.getChildren().add(boxLineName);
+        root.getChildren().add(boxLineBenutzer);
+        root.getChildren().add(boxLinePassword);
 
         Scene scene=new Scene(root, 400,400);
         primaryStage.setScene(scene);
@@ -35,5 +46,29 @@ public class passwdManagerApp extends Application {
         box.getChildren().add(lName);
         box.getChildren().add(tfName);
         return box;
+    }
+
+    public HBox getLineBenutzer(){
+        Label lBenutzer=new Label("Benutzer");
+        tfBenutzer=new TextField();
+        HBox box=new HBox();
+        box.getChildren().add(lBenutzer);
+        box.getChildren().add(tfBenutzer);
+        return box;
+    }
+
+    public HBox getLinePassword(){
+        Label lPassword=new Label("Passwort");
+        pfPassword=new PasswordField();
+        Button bEintragen=new Button("Eintragen");
+        bEintragen.setOnAction(e->eintragen(e));
+        HBox box=new HBox();
+        box.getChildren().add(lPassword);
+        box.getChildren().add(pfPassword);
+        return box;
+    }
+
+    public void eintragen(ActionEvent e){
+
     }
 }
