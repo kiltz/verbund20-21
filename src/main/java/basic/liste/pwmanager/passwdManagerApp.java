50c9f44dc75ae21da5859fdfe5be33f04f6d1fa3
pauleman1 <paul.hofmann@ingentis.com>
2021-02-02 13:18:55 +0100

    manager ala passworto

--- src/main/java/basic/liste/pwmanager/passwdManagerApp.java
+++ src/main/java/basic/liste/pwmanager/passwdManagerApp.java
@@ -2,110 +2,87 @@ package basic.liste.pwmanager;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import javax.swing.*;
+import javax.xml.soap.Text;
+import java.util.List;
+
 
 public class passwdManagerApp extends Application {
-    public TextField tfName;
-    public TextField tfUser;
-    public PasswordField pfPassword;
-    public TextField tfSearch;
-    private TextArea taOutput;
 
-    public Manager manager;
+    private TextField tfName;
+    private TextField tfUser;
+    private PasswordField pfPassword;
+    private TextField tfSearch;
+    private TextArea taOutput;
+    private Manager manager;
+
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
-    public void start(Stage primaryStage){
-        VBox root = new VBox();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-        HBox boxLineName=getLineName();
-        HBox boxLineUser=getLineUser();
-        HBox boxLinePassword=getLinePassword();
-        HBox boxLineSearch=getLineSearch();
-        HBox boxLineOutput=getLineOutput();
+        VBox vBox = new VBox(10);
+        vBox.setPadding(new Insets(20, 20, 20, 20));
 
-        root.getChildren().add(boxLineName);
-        root.getChildren().add(boxLineUser);
-        root.getChildren().add(boxLinePassword);
-        root.getChildren().add(boxLineSearch);
-        root.getChildren().add(boxLineOutput);
+        HBox hName = new HBox(10);
+        Label lName = new Label("Name: ");
+        tfName = new TextField();
+        hName.getChildren().addAll(lName, tfName);
 
-        root.setSpacing(10);
+        HBox hUser = new HBox(10);
+        Label lUser = new Label("Benutzer: ");
+        tfUser = new TextField();
+        hUser.getChildren().addAll(lUser, tfUser);
 
-        Scene scene=new Scene(root, 400,400);
+        HBox hPassword = new HBox(10);
+        Label lPassword = new Label("Passwort: ");
+        pfPassword = new PasswordField();
+        hPassword.getChildren().addAll(lPassword, pfPassword);
+
+        Button btnInput = new Button("Eintragen");
+        btnInput.setOnAction(e -> input(e));
+
+        HBox hSearch = new HBox(10);
+        tfSearch = new TextField();
+        Button btnSearch = new Button("Suchen");
+        btnSearch.setOnAction(f -> suchen(f));
+        hSearch.getChildren().addAll(tfSearch, btnSearch);
+
+        taOutput = new TextArea();
+        taOutput.setEditable(false);
+        taOutput.setPrefWidth(500);
+
+        vBox.getChildren().addAll(hName, hUser, hPassword, btnInput, hSearch, taOutput);
+
+        manager = new Manager();
+
+        Scene scene = new Scene(vBox, 500, 400);
+        primaryStage.setTitle("Passwort Manager App");
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Passwort Manager");
         primaryStage.show();
     }
 
-    public HBox getLineName(){
-        Label lName=new Label("Name");
-        tfName=new TextField();
-
-        HBox box=new HBox();
-        box.getChildren().add(lName);
-        box.getChildren().add(tfName);
-        box.setSpacing(10);
-        return box;
+    private void suchen(ActionEvent f) {
+        taOutput.setText("");
+        List<Passwort> passwords = manager.suche(tfSearch.getText());
+        for (Passwort password : passwords) {
+            taOutput.setText(taOutput.getText() + password +"\n");
+        }
     }
 
-    public HBox getLineUser(){
-        Label lBenutzer=new Label("User");
-        tfUser=new TextField();
-        HBox box=new HBox();
-        box.getChildren().add(lBenutzer);
-        box.getChildren().add(tfUser);
-        box.setSpacing(10);
-        return box;
-    }
-
-    public HBox getLinePassword(){
-        Label lPassword=new Label("Passwort");
-        pfPassword=new PasswordField();
-        Button bEintragen=new Button("Eintragen");
-        bEintragen.setOnAction(e->eintragen(e));
-        HBox box=new HBox();
-        box.getChildren().add(lPassword);
-        box.getChildren().add(pfPassword);
-        box.getChildren().add(bEintragen);
-        box.setSpacing(10);
-        return box;
-    }
-
-    public HBox getLineSearch(){
-        tfSearch=new TextField();
-        Button bSuchen=new Button("Suchen");
-        bSuchen.setOnAction(e->suchen(e));
-        HBox box=new HBox();
-        box.getChildren().add(tfSearch);
-        box.getChildren().add(bSuchen);
-        box.setSpacing(10);
-        return box;
-    }
-
-    public HBox getLineOutput(){
-        taOutput=new TextArea();
-        taOutput.setEditable(false);
-        HBox box=new HBox();
-        box.getChildren().add(taOutput);
-        box.setSpacing(10);
-        return box;
-    }
-
-    public void eintragen(ActionEvent e){
-        manager.neu(new Passwort(tfName.getText(),tfUser.getText(), pfPassword.getText()));
-    }
-    public void suchen(ActionEvent e){
-
-        manager.suche(tfSearch.getText());
+    private void input(ActionEvent e) {
+        manager.neu(new Passwort(tfName.getText(), tfUser.getText(), pfPassword.getText()));
+        tfName.setText("");
+        tfUser.setText("");
+        pfPassword.setText("");
     }
 }
-
 
