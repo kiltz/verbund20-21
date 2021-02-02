995db749f125c78412694596dc7b5f6655a8229b
Leon Wolf <leon.wolf.leon@hotmail.com>
2021-02-02 15:32:29 +0100

    Push - PropertiesApp

--- src/main/java/gui/PropertiesApp.java
+++ src/main/java/gui/PropertiesApp.java
@@ -1,16 +1,22 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.Map;
 import java.util.Properties;
 
 public class PropertiesApp extends Application {
 
-    private TextArea taAusgabe;
+    private Label lUser;
+    private Label lOS;
+    private Label lVerzeichnis;
 
     public static void main(String[] args) {
         launch();
@@ -19,12 +25,16 @@ public class PropertiesApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         VBox vMain = new VBox(10);
-        taAusgabe = new TextArea();
-        taAusgabe.setEditable(false);
-        taAusgabe.setPrefHeight(800);
+        vMain.setPadding(new Insets(20));
 
-        getProperties();
-        vMain.getChildren().add(taAusgabe);
+        lUser = new Label("User: ");
+        lOS = new Label("OS: ");
+        lVerzeichnis = new Label("Verzeichnis: ");
+
+        Button btnAnzeigen = new Button("Anzeigen");
+        btnAnzeigen.setOnAction(f -> getProperties(f));
+
+        vMain.getChildren().addAll(lUser, lOS, lVerzeichnis, btnAnzeigen);
 
         Scene scene = new Scene(vMain, 500, 400);
         primaryStage.setTitle("Dialog");
@@ -32,8 +42,17 @@ public class PropertiesApp extends Application {
         primaryStage.show();
     }
 
-    private void getProperties() {
-        Properties properties = System.getProperties();
-        properties.forEach((key, value) -> taAusgabe.setText(taAusgabe.getText() + (key + ": " + value) + "\n"));
+
+    private void getProperties(ActionEvent f) {
+        Properties props = System.getProperties();
+        for (Map.Entry<Object, Object> e : props.entrySet()) {
+            if (e.getKey().equals("os.name")) {
+                lOS.setText("OS: " + e.getValue().toString());
+            } else if (e.getKey().equals("user.name")) {
+                lUser.setText("User: " + e.getValue().toString());
+            } else if (e.getKey().equals("user.dir")) {
+                lVerzeichnis.setText("Verzeichnis: " + e.getValue().toString());
+            }
+        }
     }
 }
