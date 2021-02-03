10d80e3f8311151494854561c31bbf7bb6297ad8
AlexBleicher <alexander.bleicher@gmail.com>
2021-02-03 10:30:48 +0100

    Aufgabe IO

---
+++ src/main/java/utils/Datei.java
@@ -0,0 +1,57 @@
+package utils;
+
+import java.io.*;
+
+public class Datei extends File{
+
+
+    public Datei(String pathname) {
+        super(pathname);
+    }
+
+    public void schreibe(String line1){
+
+        try (FileWriter outStream = new FileWriter(this)) {
+
+            outStream.write(line1);
+
+        } catch (IOException e) {
+            // Fehlerbehandlung
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void schreibe(String line2, boolean isTrue){
+        try (FileWriter outStream = new FileWriter(this, isTrue)) {
+
+            outStream.write(line2);
+        } catch (IOException e) {
+            // Fehlerbehandlung
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public String lese(){
+        StringBuffer inhalt = new StringBuffer();
+        BufferedReader reader = null;
+        try (FileReader inStream = new FileReader(this)) {
+            reader = new BufferedReader(inStream);
+            String zeile = "";
+            while ((zeile = reader.readLine()) != null) // bis alles drin ist
+            {
+                inhalt.append(zeile + "\n");
+            }
+            // Ausgabe der gesammelten Werke
+            return inhalt.toString();
+        }
+        // Etwas schief gegangen?
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
