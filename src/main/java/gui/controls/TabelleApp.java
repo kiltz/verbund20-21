48845b93b797270ce97bd80b3e3fbccfafabcf14
Friedrich Kiltz <f@kiltz.de>
2021-02-03 15:34:38 +0100

    Kleine Verbesserungen

--- src/main/java/gui/controls/TabelleApp.java
+++ src/main/java/gui/controls/TabelleApp.java
@@ -5,22 +5,26 @@ import basic.liste.pwmanager.Passwort;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.util.List;
 
+/**
+ * Minimal-Version einer Tabelle.
+ */
 public class TabelleApp extends Application {
 
     private TableView<Passwort> tabelle;
     private Manager manager;
+    private TextField tfSuche;
 
     public static void main(String[] args) {
         launch(null);
@@ -30,33 +34,49 @@ public class TabelleApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         manager = new Manager();
-
-        HBox root = new HBox();
-        root.getChildren().add(getTabelle());
+        BorderPane root = new BorderPane();
+        root.setTop(getSuchLeiste());
+        root.setCenter(getTabelle());
         Scene scene = new Scene(root, 300, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    private Node getSuchLeiste() {
+        HBox box = new HBox(15);
+        box.setPadding(new Insets(10));
+        box.setAlignment(Pos.CENTER);
+        tfSuche = new TextField();
+        Button bSuche = new Button("Suche");
+        bSuche.setOnAction(e -> suche());
+        bSuche.setDefaultButton(true);
+        box.getChildren().addAll(tfSuche, bSuche);
+        return box;
+    }
+
+    private void suche() {
+        List<Passwort> liste = manager.suche(tfSuche.getText());
+        ObservableList<Passwort> pw = FXCollections
+                .observableArrayList(liste);
+        tabelle.setItems(pw);
+    }
+
+
     private Node getTabelle() {
         tabelle = new TableView<>();
         ScrollPane scrolli = new ScrollPane();
         scrolli.setContent(tabelle);
-        fuelleTabelle();
 
         TableColumn<Passwort, String> nameCol = new TableColumn<>();
         nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
         nameCol.setText("Name");
+        nameCol.setPrefWidth(120);
         TableColumn<Passwort, String> benutzerCol = new TableColumn<>();
         benutzerCol.setCellValueFactory(new PropertyValueFactory<>("benutzername"));
         benutzerCol.setText("Benutzer");
+        benutzerCol.setPrefWidth(150);
         tabelle.getColumns().addAll(nameCol, benutzerCol);
         return scrolli;
     }
 
-    private void fuelleTabelle() {
-        List<Passwort> liste = manager.suche("");
-        ObservableList<Passwort> pw = FXCollections
-                .observableArrayList(liste);
-        tabelle.setItems(pw);    }
 }
