3724c44b3466c53ec675d5459d3c205b98406c7b
AlexBleicher <alexander.bleicher@gmail.com>
2021-02-04 13:07:08 +0100

    Aufgabe pwmanager Erste Funktionalität

--- src/main/java/de/verbund/pwmanager/gui/pwmController.java
+++ src/main/java/de/verbund/pwmanager/gui/pwmController.java
@@ -3,7 +3,10 @@ package de.verbund.pwmanager.gui;
 import de.verbund.pwmanager.service.Manager;
 import de.verbund.pwmanager.service.Passwort;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.util.List;
 
 public class pwmController {
 
@@ -12,7 +15,17 @@ public class pwmController {
     public TextField tfBenutzer;
     public TextField tfPasswort;
     public Manager manager=new Manager();
+    public TextField tfSuche;
+    public TextArea taAusgabe;
+
     public void hinzufügen(ActionEvent actionEvent) throws Exception{
         manager.neu(new Passwort(tfName.getText(), tfBenutzer.getText(), tfPasswort.getText()));
+    }
+
+    public void suchen(ActionEvent actionEvent) {
+        List<Passwort> foundList=manager.suche(tfSuche.getText());
+        for(Passwort entry:foundList){
+            taAusgabe.setText(taAusgabe.getText() + "\n" + entry.getName() + ";" + entry.getBenutzername() + ";" + entry.getPasswort());
+        }
     }
 }
