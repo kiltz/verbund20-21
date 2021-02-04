4bf978b39efa21ed18ee82e28002ea36aed67c32
Mikey <mikeybarbat@gmail.com>
2021-02-04 15:54:32 +0100

    Starterapp kann schon erste Funktionen ausführen

--- src/main/java/de/verbund/pwmanager/gui/PasswortController.java
+++ src/main/java/de/verbund/pwmanager/gui/PasswortController.java
@@ -1,5 +1,6 @@
 package de.verbund.pwmanager.gui;
 
+import basic.liste.pwmanager.Manager;
 import basic.liste.pwmanager.Passwort;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,7 +9,10 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 
+import java.util.List;
+
 public class PasswortController {
+    private Manager manager;
     @FXML
     private TextField tfName;
     @FXML
@@ -30,11 +34,37 @@ public class PasswortController {
     @FXML
     private TextArea taSuchErgebnis;
 
-    public void doCreate(ActionEvent actionEvent) {
+    @FXML
+    void initialize() {
+        manager = new Manager();
+    }
 
+
+    public void doCreate(ActionEvent actionEvent) {
+        if (tfName.getText().isEmpty()) {
+            lAntwort.setText("Wen soll ich eintragen?");
+        } else {
+            try {
+                manager.neu(new Passwort(tfName.getText(), tfBenutzer.getText(), tfPasswort.getText()));
+
+                lAntwort.setText(tfName.getText()+" wurde eingetragen.");
+                tfName.setText("");
+                tfBenutzer.setText("");
+                tfPasswort.setText("");
+            } catch (Exception e) {
+
+                lAntwort.setText("Fehler: "+e.getMessage());
+            }
+        }
     }
 
     public void doSuche(ActionEvent actionEvent) {
+        List<Passwort> erg = manager.suche(tfSuche.getText());
+        StringBuffer txt = new StringBuffer("Name\tBenutzer\tPasswort\n");
+        for (Passwort p : erg) {
+            txt.append(p.getName()+"\t"+p.getBenutzername()+"\t"+p.getPasswort()+"\n");
+        }
+        taSuchErgebnis.setText(txt.toString());
     }
 
     public void doSwitch(ActionEvent actionEvent) {
