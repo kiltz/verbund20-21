beacf3fd72759f76740aab445a7de427a87fd276
AlexBleicher <alexander.bleicher@gmail.com>
2021-02-04 10:06:58 +0100

    Java fxml soweit ich kam

---
+++ src/main/java/gui/fxml/uebung/RechnerController.java
@@ -0,0 +1,16 @@
+package gui.fxml.uebung;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class RechnerController {
+
+    public TextField tf1;
+    public TextField tf2;
+    public Label lErgebnis;
+
+    public void rechne(ActionEvent e){
+        lErgebnis.setText(Integer.parseInt(tf1.getText()) + Integer.parseInt(tf2.getText()) + "");
+    }
+}
