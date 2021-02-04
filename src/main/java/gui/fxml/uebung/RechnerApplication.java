6e2c6042001d54d5c9d1285beff188c4cb3ad48d
pauleman1 <paul.hofmann@ingentis.com>
2021-02-04 10:15:42 +0100

    fxml anfangen

---
+++ src/main/java/gui/fxml/uebung/RechnerApplication.java
@@ -0,0 +1,23 @@
+package gui.fxml.uebung;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class RechnerApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception{
+        URL res=getClass().getResource("rechner.fxml");
+        Parent root= FXMLLoader.load(res);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
