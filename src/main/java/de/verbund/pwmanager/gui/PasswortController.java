e6dd6609236a0e5554617e0319fb52a7061f6227
PatrikLeg <PatrikLeggeri@web.de>
2021-02-04 15:58:12 +0100

    Funktionsweise fertig

--- src/main/java/de/verbund/pwmanager/gui/PasswortController.java
+++ src/main/java/de/verbund/pwmanager/gui/PasswortController.java
@@ -3,7 +3,10 @@ package de.verbund.pwmanager.gui;
 import de.verbund.pwmanager.service.Manager;
 import de.verbund.pwmanager.service.Passwort;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 import java.util.List;
 
@@ -14,11 +17,24 @@ public class PasswortController {
     public TextField tfName;
     public TextField tfPasswort;
     public TextField tfSuche;
+    public TextArea taListe;
+    public Label lAusgabe;
 
     private Manager manager = new Manager();
 
     public void erstellen(ActionEvent actionEvent) throws Exception {
-        manager.neu(new Passwort(tfName.getText(), tfBenutzer.getText(), tfPasswort.getText()));
+        if(tfName.getText().equals("") || tfBenutzer.getText().equals("") || tfPasswort.getText().equals("")){
+            lAusgabe.setText("Ungueltige Eingabe!");
+            lAusgabe.setTextFill(Color.RED);
+        } else {
+            manager.neu(new Passwort(tfName.getText(), tfBenutzer.getText(), tfPasswort.getText()));
+            lAusgabe.setText("Erstellen erfolgreich");
+            lAusgabe.setTextFill(Color.BLUE);
+            taListe.setText("");
+            tfName.setText("");
+            tfBenutzer.setText("");
+            tfPasswort.setText("");
+        }
     }
 
     public void suche(ActionEvent actionEvent) {
@@ -27,7 +43,23 @@ public class PasswortController {
         for (de.verbund.pwmanager.service.Passwort p : erg) {
             txt.append(p.getName()+"\t"+p.getBenutzername()+"\t"+p.getPasswort()+"\n");
         }
-        //taErgebnis.setText(txt.toString());
-        System.out.println(txt.toString());
+        taListe.setText(txt.toString());
+        lAusgabe.setText("");
+
+    }
+
+    public void pwAendern(ActionEvent actionEvent)throws Exception {
+        if(tfName.getText().equals("") || tfBenutzer.getText().equals("") || tfPasswort.getText().equals("")){
+            lAusgabe.setText("Ungueltige Eingabe!");
+            lAusgabe.setTextFill(Color.RED);
+        } else {
+            manager.neu(new Passwort(tfName.getText(), tfBenutzer.getText(), tfPasswort.getText()));
+            lAusgabe.setText("Passwort erfolgreich geändert");
+            lAusgabe.setTextFill(Color.BLUE);
+            taListe.setText("");
+            tfName.setText("");
+            tfBenutzer.setText("");
+            tfPasswort.setText("");
+        }
     }
 }
