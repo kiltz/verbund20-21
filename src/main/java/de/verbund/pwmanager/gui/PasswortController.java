161be60205985dde5fc76332d2fcd2c4ba08428e
Mikey <mikeybarbat@gmail.com>
2021-02-05 11:47:40 +0100

    Starterapp kann schon erste Funktionen ausführen + kleine verbesserungen am Design

--- src/main/java/de/verbund/pwmanager/gui/PasswortController.java
+++ src/main/java/de/verbund/pwmanager/gui/PasswortController.java
@@ -7,9 +7,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class PasswortController {
     private Manager manager;
@@ -41,7 +44,6 @@ public class PasswortController {
         manager = new Manager();
     }
 
-
     public void doCreate(ActionEvent actionEvent) {
         if (tfName.getText().isEmpty()) {
             lAntwort.setText("Wen soll ich eintragen?");
@@ -49,13 +51,13 @@ public class PasswortController {
             try {
                 manager.neu(new Passwort(tfName.getText(), tfBenutzer.getText(), tfPasswort.getText()));
 
-                lAntwort.setText(tfName.getText()+" wurde eingetragen.");
+                lAntwort.setText(tfName.getText() + " wurde eingetragen.");
                 tfName.setText("");
                 tfBenutzer.setText("");
                 tfPasswort.setText("");
             } catch (Exception e) {
 
-                lAntwort.setText("Fehler: "+e.getMessage());
+                lAntwort.setText("Fehler: " + e.getMessage());
             }
         }
     }
@@ -64,7 +66,7 @@ public class PasswortController {
         List<Passwort> erg = manager.suche(tfSuche.getText());
         StringBuffer txt = new StringBuffer("Name\tBenutzer\tPasswort\n");
         for (Passwort p : erg) {
-            txt.append(p.getName()+"\t"+p.getBenutzername()+"\t"+p.getPasswort()+"\n");
+            txt.append(p.getName() + "\t" + p.getBenutzername() + "\t" + p.getPasswort() + "\n");
         }
         taSuchErgebnis.setText(txt.toString());
     }
@@ -76,14 +78,24 @@ public class PasswortController {
             try {
                 manager.neu(new Passwort(tfWechselName.getText(), tfWechselBenutzer.getText(), tfNeuPasswort.getText()));
 
-                lWechselAntwort.setText(tfWechselName.getText()+" wurde eingetragen.");
+                lWechselAntwort.setText(tfWechselName.getText() + " wurde eingetragen.");
                 tfWechselName.setText("");
                 tfWechselBenutzer.setText("");
                 tfNeuPasswort.setText("");
             } catch (Exception e) {
 
-                lWechselAntwort.setText("Fehler: "+e.getMessage());
+                lWechselAntwort.setText("Fehler: " + e.getMessage());
             }
         }
+    }
+
+    public void doAusfuellen(KeyEvent keyEvent) {
+        List<Passwort> erg = manager.suche(tfName.getText());
+        if(erg.size()==1){
+
+            tfName.setText("");
+            tfBenutzer.setText("");
+        }
+        System.out.println("Anzahl Treffer: " + erg.size());
     }
 }
