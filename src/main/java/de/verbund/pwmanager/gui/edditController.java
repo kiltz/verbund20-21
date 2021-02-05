95869c70aaa101c99f2a4c687e20d8f4f3d15c4e
AlexBleicher <alexander.bleicher@gmail.com>
2021-02-05 13:56:56 +0100

    Anfang edditieren

--- src/main/java/de/verbund/pwmanager/gui/edditController.java
+++ src/main/java/de/verbund/pwmanager/gui/edditController.java
@@ -5,14 +5,20 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
 public class edditController {
-
+    public Passwort passwort= pwmController.passwort;
     public TextField tfNameEddit;
     public TextField tfBenutzerEddit;
     public TextField tfPasswortEddit;
 
-    public Passwort passwort= pwmController.passwort;
 
 
     public void ändern(ActionEvent actionEvent) {
+
+    }
+
+    public void zeigeAktuell(ActionEvent actionEvent) {
+        tfNameEddit.setText(passwort.getName());
+        tfBenutzerEddit.setText(passwort.getBenutzername());
+        tfPasswortEddit.setText(passwort.getPasswort());
     }
 }
